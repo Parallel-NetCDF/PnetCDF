180d50c96523bec2782677edfdd26b7c8cabb2b9
William Gropp <wgropp@users.noreply.github.com>
2003-02-21 16:04:11 +0000

    Add the generated Fortran interface files to make it easier to checkout the sources
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@49 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

---
+++ src/libf/mpinetcdf_impl.h
@@ -0,0 +1,66 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*  
+ *  (C) 2003 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+#ifndef MPINETCDF_IMPL_H
+#define MPINETCDF_IMPL_H
+#include "../lib/ncconfig.h"
+#include "../lib/mpinetcdf.h"
+
+/* Support Windows extension to specify calling convention */
+#ifdef USE_FORT_STDCALL
+#define FORT_CALL __stdcall
+#elif defined (USE_FORT_CDECL)
+#define FORT_CALL __cdecl
+#else
+#define FORT_CALL
+#endif
+
+/* Handle different mechanisms for passing Fortran CHARACTER to routines */
+#ifdef USE_FORT_MIXED_STR_LEN
+#define FORT_MIXED_LEN_DECL   , MPI_Fint
+#define FORT_END_LEN_DECL
+#define FORT_MIXED_LEN(a)     , MPI_Fint a
+#define FORT_END_LEN(a)
+#else
+#define FORT_MIXED_LEN_DECL
+#define FORT_END_LEN_DECL     , MPI_Fint
+#define FORT_MIXED_LEN(a)
+#define FORT_END_LEN(a)       , MPI_Fint a
+#endif
+
+/* Support Windows extension to specify which functions are exported from
+   shared (DLL) libraries */
+#ifdef HAVE_FORTRAN_API
+# ifdef FORTRAN_EXPORTS
+#  define FORTRAN_API __declspec(dllexport)
+# else
+#  define FORTRAN_API __declspec(dllimport)
+# endif
+#else
+# define FORTRAN_API
+#endif
+
+/* Support an alternate approach for declaring a weak symbol supported by
+   some versions of gcc */
+#ifdef USE_WEAK_ATTRIBUTE
+#define FUNC_ATTRIBUTES(name) __attribute__ ((weak,alias(#name)))
+#else
+#define FUNC_ATTRIBUTES(name)
+#endif
+
+/* Utility functions */
+
+/* Define the internal values needed for Fortran support */
+
+/* Fortran logicals */
+
+/* Fortran logical values */
+
+#endif
+
+
+
+
