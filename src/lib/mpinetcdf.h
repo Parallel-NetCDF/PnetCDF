8de677c667799da2fb347772651844f7e1e76ba3
William Gropp <wgropp@users.noreply.github.com>
2003-02-21 14:41:31 +0000

    Added Fortran name mapping tests to configure and started on Fortran interface
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@45 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- src/lib/mpinetcdf.h
+++ src/lib/mpinetcdf.h
@@ -1,20 +1,14 @@
 #include <mpi.h>
 #include "nc.h"
 
-int
-ncmpi_create(MPI_Comm comm, const char *path, int cmode, MPI_Info info, int *ncidp);
+/* Begin Prototypes */
+int ncmpi_create(MPI_Comm comm, const char *path, int cmode, MPI_Info info, int *ncidp);
 
-int
-ncmpi_open(MPI_Comm comm, const char *path, int omode, MPI_Info info, int *ncidp);
+int ncmpi_open(MPI_Comm comm, const char *path, int omode, MPI_Info info, int *ncidp);
 
+int ncmpi_enddef(int ncid);
 
-int
-ncmpi_enddef(int ncid);
-
-
-
-int
-ncmpi_close(int ncid);
+int ncmpi_close(int ncid);
 
 /* Begin _dim */
 
@@ -30,65 +24,54 @@ ncmpi_close(int ncid);
 /* End _var */
 /* Begin {put,get}_vara */
 
-int
-ncmpi_put_vara_int_all(int ncid, int varid,
-                    const size_t start[], const size_t count[],
+int ncmpi_put_vara_int_all(int ncid, int varid,
+                    const size_t [], const size_t [],
                     const int *op);
 
-int
-ncmpi_put_vara_int(int ncid, int varid,
-                const size_t start[], const size_t count[],
+int ncmpi_put_vara_int(int ncid, int varid,
+                const size_t [], const size_t [],
                 const int *op);
 
-int
-ncmpi_put_vara_float_all(int ncid, int varid,
-                    const size_t start[], const size_t count[],
+int ncmpi_put_vara_float_all(int ncid, int varid,
+                    const size_t [], const size_t [],
                     const float *op);
 
-int
-ncmpi_put_vara_float(int ncid, int varid,
-                const size_t start[], const size_t count[],
+int ncmpi_put_vara_float(int ncid, int varid,
+                const size_t [], const size_t [],
                 const float *op);
 
-int
-ncmpi_put_vara_double_all(int ncid, int varid,
-                    const size_t start[], const size_t count[],
+int ncmpi_put_vara_double_all(int ncid, int varid,
+                    const size_t [], const size_t [],
                     const double *op);
 
-int
-ncmpi_put_vara_double(int ncid, int varid,
-                const size_t start[], const size_t count[],
+int ncmpi_put_vara_double(int ncid, int varid,
+                const size_t [], const size_t [],
                 const double *op);
 
-int
-ncmpi_get_vara_int_all(int ncid, int varid,
-                    const size_t start[], const size_t count[],
+int ncmpi_get_vara_int_all(int ncid, int varid,
+                    const size_t [], const size_t [],
                     int *ip);
 
-int
-ncmpi_get_vara_int(int ncid, int varid,
-                const size_t start[], const size_t count[],
+int ncmpi_get_vara_int(int ncid, int varid,
+                const size_t [], const size_t [],
                 int *ip);
 
-int
-ncmpi_get_vara_float_all(int ncid, int varid,
-                    const size_t start[], const size_t count[],
+int ncmpi_get_vara_float_all(int ncid, int varid,
+                    const size_t [], const size_t [],
                     float *ip);
 
-int
-ncmpi_get_vara_float(int ncid, int varid,
-                const size_t start[], const size_t count[],
+int ncmpi_get_vara_float(int ncid, int varid,
+                const size_t [], const size_t [],
                 float *ip);
 
-int
-ncmpi_get_vara_double_all(int ncid, int varid,
-                    const size_t start[], const size_t count[],
+int ncmpi_get_vara_double_all(int ncid, int varid,
+                    const size_t [], const size_t [],
                     double *ip);
 
-int
-ncmpi_get_vara_double(int ncid, int varid,
-                const size_t start[], const size_t count[],
+int ncmpi_get_vara_double(int ncid, int varid,
+                const size_t [], const size_t [],
                 double *ip);
 
+/* End Prototypes */
 
 /* End {put,get}_vara */
