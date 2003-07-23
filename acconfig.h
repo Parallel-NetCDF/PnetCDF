6503d0e26d3a7f0b16b8b7749ec30bbd65b5ef8a
Rob Latham <rlatham@gmail.com>
2003-07-23 21:31:00 +0000

    do more fortran magic
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@127 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

---
+++ acconfig.h
@@ -0,0 +1,16 @@
+#undef HAVE_FTRUNCATE
+#undef NCBYTE_T
+#undef NCSHORT_T
+#undef NF_DOUBLEPRECISION_IS_C_
+#undef NF_INT1_IS_C_
+#undef NF_INT1_T
+#undef NF_INT2_IS_C_
+#undef NF_INT2_T
+#undef NF_INT_IS_C_
+#undef NF_REAL_IS_C_
+#undef NO_IEEE_FLOAT
+#undef NO_STDLIB_H
+#undef NO_STRERROR
+#undef NO_SYS_TYPES_H
+#undef SIZEOF_OFF_T
+#undef SIZEOF_SIZE_T
