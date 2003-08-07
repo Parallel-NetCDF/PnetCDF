6f86ae41cbceb0b36c45057908dcce9a09909b6e
William Gropp <wgropp@users.noreply.github.com>
2003-08-07 15:33:21 +0000

    Added interface to strerror
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@189 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

---
+++ src/libf/strerrorf.f
@@ -0,0 +1,8 @@
+      character *(80) nfmpi_strerror( err )
+      integer err, ierr
+      character *(80) tmpstr
+C      
+C     Call a (C) routine that places the message into tmpstr
+      call nfmpi_xstrerror( err, tmpstr, ierr )
+      nfmpi_strerror = tmpstr
+      end
