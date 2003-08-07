4a5bc07d73406e1f8c7c23d8d56338b21c04c6d1
Rob Latham <rlatham@gmail.com>
2003-08-07 18:36:27 +0000

    our own version of the serial netcdf library of utility functions
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@191 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- test/nf_test/fortlib.c
+++ test/nf_test/fortlib.c
@@ -4,6 +4,9 @@
  * This file contains support functions for FORTRAN code.  For example,
  * under HP-UX A.09.05, the U77 library doesn't contain the exit()
  * routine -- so we create one here.
+ *
+ * This file is heavily modified from nf_test/fortlib.c in the serial netcdf
+ * source.
  */
 
 
@@ -11,136 +14,83 @@
 #include <limits.h>
 #include <float.h>
 
-#include "../fortran/ncfortran.h"
 
-#if defined(f2cFortran)
+#ifdef F77_NAME_UPPER
+#define ud_exit_ UD_EXIT
+#elif defined(F77_NAME_LOWER_2USCORE)
+#define ud_exit_  ud_exit__
+#elif !defined(F77_NAME_LOWER_USCORE)
+#define ud_exit_  ud_exit
+/* Else leave name alone */
+#endif
+
+FORTRAN_API void FORT_CALL ud_exit_(int *v1) {
+	exit (*v1);
+	return;
+}
+
+#ifdef F77_NAME_UPPER
+#define ud_abort_ UD_ABORT
+#elif defined(F77_NAME_LOWER_2USCORE)
+#define ud_abort_  ud_abort__
+#elif !defined(F77_NAME_LOWER_USCORE)
+#define ud_abort_  ud_abort
+/* Else leave name alone */
+#endif
+
+FORTRAN_API void FORT_CALL ud_abort_(int *v1) {
+	abort();
+	return;
+}
+
+#ifdef F77_NAME_UPPER
+#define ud_rand_ UD_RAND
+#elif defined(F77_NAME_LOWER_2USCORE)
+#define ud_rand_  ud_rand__
+#elif !defined(F77_NAME_LOWER_USCORE)
+#define ud_rand_  ud_rand
+/* Else leave name alone */
+#endif
+
 /*
- * The f2c(1) utility on BSD/OS and Linux systems adds an additional
- * underscore suffix (besides the usual one) to global names that have
- * an embedded underscore.  For example, `nfclose' becomes `nfclose_',
- * but `nf_close' becomes `nf_close__.  Consequently, we have to modify
- * some names.
- */
-#define max_uchar	max_uchar_
-#define min_schar	min_schar_
-#define max_schar	max_schar_
-#define min_short	min_short_
-#define max_short	max_short_
-#define min_int		min_int_
-#define max_int		max_int_
-#define min_long	min_long_
-#define max_long	max_long_
-#define max_float	max_float_
-#define max_double	max_double_
-#endif	/* f2cFortran */
-
-
-FCALLSCSUB1(exit, UDEXIT, udexit, FINT2CINT)
-
-
-FCALLSCSUB0(abort, UDABORT, udabort)
-
-
-static double
-myrand(int iflag)
-{
-    if (iflag != 0)
-	srand(iflag);
-
-    /*
-     * Return a pseudo-random value between 0.0 and 1.0.
-     *
-     * We don't use RAND_MAX here because not all compilation
-     * environments define it (e.g. gcc(1) under SunOS 4.1.3).
-     */
-    return (rand() % 32768) / 32767.0;
+* Return a pseudo-random value between 0.0 and 1.0.
+*
+* We don't use RAND_MAX here because not all compilation
+* environments define it (e.g. gcc(1) under SunOS 4.1.3).
+*/
+FORTRAN_API double FORT_CALL ud_rand_(int *seed) {
+	if (*seed != 0)
+		srand(*seed);
+	return (rand() % 32768 )/ 32767.0;
 }
-FCALLSCFUN1(DOUBLE, myrand, UDRAND, udrand, FINT2CINT)
 
+#ifdef F77_NAME_UPPER
+#define ud_shift_ UD_SHIFT
+#elif defined(F77_NAME_LOWER_2USCORE)
+#define ud_shift_  ud_shift__
+#elif !defined(F77_NAME_LOWER_USCORE)
+#define ud_shift_  ud_shift
+/* Else leave name alone */
+#endif
 
-static int
-myshift(int value, int amount)
-{
-    if (amount < 0)
-	value >>= -amount;
-    else
-    if (amount > 0)
-	value <<= amount;
-    return value;
+FORTRAN_API int FORT_CALL ud_shift_(int * value, int *amount) {
+	if(*amount < 0)
+		*value >>= -*amount;
+	else if (*amount > 0)
+		*value <<= *amount;
+	return value;
 }
-FCALLSCFUN2(NF_INT, myshift, UDSHIFT, udshift, FINT2CINT, FINT2CINT)
-
+#ifdef F77_NAME_UPPER
+#define nc_ignorefpe_ UD_SHIFT
+#elif defined(F77_NAME_LOWER_2USCORE)
+#define nc_ignorefpe_  ud_shift__
+#elif !defined(F77_NAME_LOWER_USCORE)
+#define nc_ignorefpe_  ud_shift
+/* Else leave name alone */
+#endif
 #include <signal.h>
-static void
-nc_ignorefpe(int doit)
+FORTRAN_API void FORT_CALL nc_ignorefpe_(int *doit)
 {
 	if(doit)
 		(void) signal(SIGFPE, SIG_IGN);
 }
-FCALLSCSUB1(nc_ignorefpe, IGNOREFPE, ignorefpe, FINT2CINT)
-
-double cmax_uchar()
-{
-    return UCHAR_MAX;
-}
-FCALLSCFUN0(DOUBLE, cmax_uchar, MAX_UCHAR, max_uchar)
-
-double cmin_schar()
-{
-    return SCHAR_MIN;
-}
-FCALLSCFUN0(DOUBLE, cmin_schar, MIN_SCHAR, min_schar)
-
-double cmax_schar()
-{
-    return SCHAR_MAX;
-}
-FCALLSCFUN0(DOUBLE, cmax_schar, MAX_SCHAR, max_schar)
-
-double cmin_short()
-{
-    return SHRT_MIN;
-}
-FCALLSCFUN0(DOUBLE, cmin_short, MIN_SHORT, min_short)
-
-double cmax_short()
-{
-    return SHRT_MAX;
-}
-FCALLSCFUN0(DOUBLE, cmax_short, MAX_SHORT, max_short)
-
-double cmin_int()
-{
-    return INT_MIN;
-}
-FCALLSCFUN0(DOUBLE, cmin_int, MIN_INT, min_int)
-
-double cmax_int()
-{
-    return INT_MAX;
-}
-FCALLSCFUN0(DOUBLE, cmax_int, MAX_INT, max_int)
-
-double cmin_long()
-{
-    return LONG_MIN;
-}
-FCALLSCFUN0(DOUBLE, cmin_long, MIN_LONG, min_long)
-
-double cmax_long()
-{
-    return LONG_MAX;
-}
-FCALLSCFUN0(DOUBLE, cmax_long, MAX_LONG, max_long)
-
-double cmax_float()
-{
-    return FLT_MAX;
-}
-FCALLSCFUN0(DOUBLE, cmax_float, MAX_FLOAT, max_float)
-
-double cmax_double()
-{
-    return DBL_MAX;
-}
-FCALLSCFUN0(DOUBLE, cmax_double, MAX_DOUBLE, max_double)
