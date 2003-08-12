51201c889a54539bb0118d3b86b7534cdd8e2d97
Rob Ross <unknown@users.noreply.github.com>
2003-08-12 14:56:12 +0000

    name mangling fix on fortran side of things.
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@218 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- src/libf/nfxutil.c
+++ src/libf/nfxutil.c
@@ -5,7 +5,7 @@ int ncxVardim( int ncid, int varid )
   NC_var *varp;
   NC     *ncp;
 
-  NC_check_id(ncid, &ncp);
-  varp = NC_lookupvar(ncp, varid);
+  ncmpii_NC_check_id(ncid, &ncp);
+  varp = ncmpii_NC_lookupvar(ncp, varid);
   return varp->ndims;
 }
