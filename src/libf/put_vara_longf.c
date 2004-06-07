5c9f39bbf4eded0563231b8bdad1d3f8357a1217
Rob Latham <rlatham@gmail.com>
2004-06-07 16:39:55 +0000

    regenerate to fixup prototypes ofr _longf routines
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@391 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- src/libf/put_vara_longf.c
+++ src/libf/put_vara_longf.c
@@ -21,7 +21,7 @@
 
 /* Prototypes for the Fortran interfaces */
 #include "mpifnetcdf.h"
-FORTRAN_API int FORT_CALL nfmpi_put_vara_long_ ( int *v1, int *v2, int v3[], int v4[], long*v5 ){
+FORTRAN_API int FORT_CALL nfmpi_put_vara_long_ ( int *v1, int *v2, MPI_Offset v3[], MPI_Offset v4[], long*v5 ){
     int ierr;
     ierr = ncmpi_put_vara_long( *v1, *v2, (const MPI_Offset *)(v3), (const MPI_Offset *)(v4), v5 );
     return ierr;
