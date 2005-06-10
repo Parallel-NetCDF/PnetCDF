66a3518df36a73df7de1c04e193ad5281c36ed75
Rob Latham <rlatham@gmail.com>
2005-06-10 16:37:52 +0000

    bump version for prerelease
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@472 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- src/lib/mpinetcdf.c
+++ src/lib/mpinetcdf.c
@@ -22,7 +22,7 @@ static int length_of_mpitype(MPI_Datatype);
 
 const char *
 ncmpi_inq_libvers(void) {
-  return "version = 1.0.0 of XXX";
+  return "version = 1.0.0-pre3 of 10 JUN 2005";
 }
 
 /* Prototypes for functions used only in this file */
