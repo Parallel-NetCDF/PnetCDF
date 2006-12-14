afa2045002c444fd30cea2131e374390bbc0f39b
Rob Latham <rlatham@gmail.com>
2006-12-14 20:58:47 +0000

    abandon the autoconf-2.13-style AC_CHECK_TYPE approach of #defining types. Use instead  the autoconf-2.50-style AC_CHECK_TYPES approach: define a typedef if HAVE_SOME_TYPE not defined.
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@515 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

---
+++ src/lib/nctypes.h
@@ -0,0 +1,23 @@
+/*****************************************************************************
+ *
+ * This file is created by Northwestern University and Argonne National
+ * Laboratory
+ *
+ ****************************************************************************/
+
+#ifndef NCTYPES_H
+#define NCTYPES_H
+
+#ifndef HAVE_PTRDIFF_T
+typedef int ptrdiff_t;
+#endif
+
+#ifndef HAVE_SSIZE_T
+typedef int ssize_t;
+#endif
+
+#ifndef HAVE_UCHAR
+typedef unsigned char uchar;
+#endif
+
+#endif
