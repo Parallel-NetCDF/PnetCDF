b7416eea11838976d9824af54975b8355b9dc1fd
Rob Latham <rlatham@gmail.com>
2007-08-06 21:31:23 +0000

    regenerate bindings after wei-keng's changes
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@557 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- src/libf/inq_dimlenf.c
+++ src/libf/inq_dimlenf.c
@@ -21,7 +21,7 @@
 
 /* Prototypes for the Fortran interfaces */
 #include "mpifnetcdf.h"
-FORTRAN_API int FORT_CALL nfmpi_inq_dimlen_ ( int *v1, int *v2, MPI_Offset*v3 ){
+FORTRAN_API int FORT_CALL nfmpi_inq_dimlen_ ( int *v1, int *v2, MPI_Fint *v3 ){
     int ierr;
     int l2 = *v2 - 1;
     ierr = ncmpi_inq_dimlen( *v1, l2, v3 );
