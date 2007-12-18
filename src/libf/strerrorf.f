de6afc87bb6b2c5d6fc5771594a7cfb2b5ab28af
Rob Latham <rlatham@gmail.com>
2007-12-18 21:24:16 +0000

    put the function declarations in the wrong place apparently
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@602 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- src/libf/strerrorf.f
+++ src/libf/strerrorf.f
@@ -1,9 +1,9 @@
-      integer nfmpi_xstrerror
-      external nfmpi_xstrerror
-
       character *80 function nfmpi_strerror( err )
       integer err, ierr
       character *(80) tmpstr
+
+      integer nfmpi_xstrerror
+      external nfmpi_xstrerror
 C      
 C     Call a (C) routine that places the message into tmpstr
       ierr = nfmpi_xstrerror( err, tmpstr )
