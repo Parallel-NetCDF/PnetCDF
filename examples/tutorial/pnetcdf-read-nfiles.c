ff691cd4f5638f02e7044eaf8fee0f6250071270
Rob Latham <rlatham@gmail.com>
2012-02-19 01:19:43 +0000

    forgot to add these
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@971 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

---
+++ examples/tutorial/pnetcdf-read-nfiles.c
@@ -0,0 +1,124 @@
+/* simple demonstration of pnetcdf 
+ * text attribute on dataset
+ * rank 0 reads into 1-d array, broadcasts to all.  This is a dumb way
+ * to do parallel I/O but folks do this sometimes... */
+
+#include <stdlib.h>
+#include <mpi.h>
+#include <pnetcdf.h>
+#include <stdio.h>
+
+static void handle_error(int status)
+{
+	fprintf(stderr, "%s\n", ncmpi_strerror(status));
+	exit(-1);
+}
+
+#define DSET_NAME_LEN 1024
+
+int main(int argc, char **argv) {
+
+    int rank, nprocs;
+    int ret, ncfile, ndims, nvars, ngatts, unlimited;
+    int var_ndims, var_natts;;
+    MPI_Offset *dim_sizes, var_size;
+    MPI_Offset *count;
+
+    char varname[NC_MAX_NAME+1];
+    int dimids[NC_MAX_VAR_DIMS];
+    nc_type type;
+    char filename[DSET_NAME_LEN];
+
+    int i, j;
+
+    int *data;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    /* the most significant challenge with the "one file per processor"
+     * approach is the challenge in reading back on a different number of
+     * processors.  For example, 4k processors output data during a simulation.
+     * Later on, 100 processors do analysis or visualization of the data.
+     * Stiching together the many smaller files into a form usable by other
+     * programs poses a challenge */
+
+    ret = snprintf(filename, DSET_NAME_LEN, "%s.%d-%d.nc", argv[1], rank, nprocs);
+    if (ret >= DSET_NAME_LEN) {
+	    fprintf(stderr, "name too long \n");
+	    exit(-1);
+    }
+    ret = ncmpi_open(MPI_COMM_SELF, filename, NC_NOWRITE, MPI_INFO_NULL,
+		    &ncfile);
+    if (ret != NC_NOERR) handle_error(ret);
+
+    /* reader knows nothing about dataset, but we can interrogate with query
+     * routines: ncmpi_inq tells us how many of each kind of "thing"
+     * (dimension, variable, attribute) we will find in the file  */
+
+    /* In the "one file per processor case" all processors open a file and
+     * interrogate it */
+
+    ret = ncmpi_inq(ncfile, &ndims, &nvars, &ngatts, &unlimited);
+    if (ret != NC_NOERR) handle_error(ret);
+
+    /* we do not really need the name of the dimension or the variable for
+     * reading in this example.  we could, in a different example, take the
+     * name of a variable on the command line and read just that one */
+
+    dim_sizes = calloc(ndims, sizeof(MPI_Offset));
+    /* netcdf dimension identifiers are allocated sequentially starting
+     * at zero; same for variable identifiers */
+    for(i=0; i<ndims; i++)  {
+	    ret = ncmpi_inq_dimlen(ncfile, i, &(dim_sizes[i]) );
+	    if (ret != NC_NOERR) handle_error(ret);
+    }
+
+
+    for(i=0; i<nvars; i++) { 
+	    /* much less coordination in this case compared to rank 0 doing all
+	     * the i/o: everyone already has the necessary information */
+	    ret = ncmpi_inq_var(ncfile, i, varname, &type, &var_ndims, dimids,
+			    &var_natts);
+	    if (ret != NC_NOERR) handle_error(ret);
+
+	    count = calloc(var_ndims, sizeof(MPI_Offset));
+
+	    /* as long as the number of readers is identical to the number of
+	     * writers, we can simply read entire variables back */
+
+	    count[0] = dim_sizes[dimids[0]];
+
+	    var_size = count[0];
+	    for (j=1; j<var_ndims; j++) {
+		    count[j] = dimids[j];
+		    var_size *= count[j];
+	    }
+
+	    switch(type) {
+		    case NC_INT:
+			    data = calloc(var_size, sizeof(int));
+			    ret = ncmpi_get_var_int_all(ncfile, i, data);
+			    if (ret != NC_NOERR) handle_error(ret);
+			    break;
+		    default:
+			    /* we can do this for all the known netcdf types but this
+			     * example is already getting too long  */
+			    fprintf(stderr, "unsupported NetCDF type \n");
+	    }
+
+	    free(count);
+	    if (data != NULL) free(data);
+    }
+
+    ret = ncmpi_close(ncfile);
+    if (ret != NC_NOERR) handle_error(ret);
+
+    MPI_Finalize();
+    return 0;
+}
+
+/*
+ *vim: ts=8 sts=4 sw=4 noexpandtab */
---
+++ examples/tutorial/pnetcdf-write-nfiles.c
@@ -0,0 +1,93 @@
+/* simple demonstration of pnetcdf 
+ * text attribute on dataset
+ * write out rank into 1-d array collectively.  The most basic way to do
+ * parallel i/o with pnetcdf */
+
+#include <stdlib.h>
+#include <mpi.h>
+#include <pnetcdf.h>
+#include <stdio.h>
+
+static void handle_error(int status)
+{
+	fprintf(stderr, "%s\n", ncmpi_strerror(status));
+	exit(-1);
+}
+
+#define DSET_NAME_LEN 1024
+
+int main(int argc, char **argv) {
+
+	int ret, ncfile, nprocs, rank, dimid, varid1, varid2, ndims=1;
+	MPI_Offset count=1;
+	char buf[13] = "Hello World\n";
+	int data;
+	char filename[DSET_NAME_LEN];
+
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+	/* Many applications find "one file per process" easy, but there are
+	 * several deficincies with that approach:
+	 * - here we need to construct a unique file name for each processor */
+	ret = snprintf(filename, DSET_NAME_LEN, "%s.%d-%d.nc", argv[1], rank, nprocs);
+	if (ret >= DSET_NAME_LEN) {
+		fprintf(stderr, "name too long \n");
+		exit(-1);
+	}
+
+	/* note that the communicator is still needed but now it is
+	 * MPI_COMM_SELF: since each processor opens its own file we cannot use
+	 * MPI_COMM_WORLD */
+	ret = ncmpi_create(MPI_COMM_SELF, filename,
+			    NC_WRITE|NC_64BIT_OFFSET, MPI_INFO_NULL, &ncfile);
+	if (ret != NC_NOERR) handle_error(ret);
+
+	/* each processor writes its data to a file, so instead of an "nprocs"
+	 * sized array, we just have an array big enough to hold one
+	 * processor's data */
+	ret = ncmpi_def_dim(ncfile, "d1", 1, &dimid);
+	if (ret != NC_NOERR) handle_error(ret);
+
+	ret = ncmpi_def_var(ncfile, "v1", NC_INT, ndims, &dimid, &varid1);
+	if (ret != NC_NOERR) handle_error(ret);
+
+	ret = ncmpi_def_var(ncfile, "v2", NC_INT, ndims, &dimid, &varid2);
+	if (ret != NC_NOERR) handle_error(ret);
+
+	ret = ncmpi_put_att_text(ncfile, NC_GLOBAL, "string", 13, buf);
+	if (ret != NC_NOERR) handle_error(ret);
+
+	/* ncmpi_enddef writes the header out as in other examples, but because
+	 * each processor opened the file independently, there can be no "write
+	 * and broadcast" optimization.  Instead, every processor does header
+	 * i/o.  */	
+	ret = ncmpi_enddef(ncfile); if (ret != NC_NOERR) handle_error(ret);
+
+	/* the one advantage to this approach: data decomposistion is easy the
+	 * application does not need to worry aobut the shape and location of
+	 * the data (the 'start' and 'count' parmeters in the 'vara' family of
+	 * functions) and can instead just write the enitre (small) variable */
+
+	data=rank;
+
+	/* in this simple example every process writes its rank to two 1d
+	 * variables */
+	/* When each processor writes to its own file, a whole host of
+	 * optimizations cannot take place.   */
+	
+	ret = ncmpi_put_var_int_all(ncfile, varid1, &data);
+	if (ret != NC_NOERR) handle_error(ret);
+
+	ret = ncmpi_put_var_int_all(ncfile, varid2, &data);
+	if (ret != NC_NOERR) handle_error(ret);
+
+	ret = ncmpi_close(ncfile);
+	if (ret != NC_NOERR) handle_error(ret);
+
+	MPI_Finalize();
+
+	return 0;
+}
