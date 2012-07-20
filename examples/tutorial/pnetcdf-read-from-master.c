283372259bb396418a6a7ae796f4a87a1f1efc5b
Wei-keng Liao <wkliao@users.noreply.github.com>
2012-07-20 22:01:31 +0000

    test argc, add file contents as comments
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@1080 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- examples/tutorial/pnetcdf-read-from-master.c
+++ examples/tutorial/pnetcdf-read-from-master.c
@@ -8,114 +8,135 @@
 #include <pnetcdf.h>
 #include <stdio.h>
 
-static void handle_error(int status)
+/* This program reads a file created by pnetcdf-write-from-master.c, say file
+   named output.nc with the following contents, shown by running ncmpidump command .
+
+    % ncmpidump output.nc 
+    netcdf testfile {
+    // file format: CDF-2 (large file)
+    dimensions:
+            d1 = 4 ;
+    variables:
+            int v1(d1) ;
+        int v2(d1) ;
+
+    // global attributes:
+                :string = "Hello World\n",
+        "" ;
+    data:
+
+         v1 = 0, 1, 2, 3 ;
+
+         v2 = 0, 1, 2, 3 ;
+    }
+*/
+
+static void handle_error(int status, int lineno)
 {
-	fprintf(stderr, "%s\n", ncmpi_strerror(status));
-	exit(-1);
+    fprintf(stderr, "Error at line %d: %s\n", lineno, ncmpi_strerror(status));
+    MPI_Abort(MPI_COMM_WORLD, 1);
 }
 
 
 int main(int argc, char **argv) {
 
-    int rank, nprocs;
-    int ret, ncfile, ndims, nvars, ngatts, unlimited;
-    int var_ndims, var_natts;;
-    MPI_Offset *dim_sizes, var_size;
-
-    char varname[NC_MAX_NAME+1];
+    int i, j, rank, nprocs, ret;
+    int ncfile, ndims, nvars, ngatts, unlimited, var_ndims, var_natts;;
     int dimids[NC_MAX_VAR_DIMS];
+    char varname[NC_MAX_NAME+1];
+    MPI_Offset *dim_sizes, var_size;
     nc_type type;
-
-    int i, j;
-
     int *data;
-
 
     MPI_Init(&argc, &argv);
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
+    if (argc != 2) {
+        if (rank == 0) printf("Usage: %s filename\n", argv[0]);
+        MPI_Finalize();
+        exit(-1);
+    }
+
     if (rank == 0) {
-	ret = ncmpi_open(MPI_COMM_SELF, argv[1],
-		NC_NOWRITE, MPI_INFO_NULL, &ncfile);
-	if (ret != NC_NOERR) handle_error(ret);
+        ret = ncmpi_open(MPI_COMM_SELF, argv[1],
+                         NC_NOWRITE, MPI_INFO_NULL, &ncfile);
+        if (ret != NC_NOERR) handle_error(ret, __LINE__);
 
-	/* reader knows nothing about dataset, but we can interrogate with
-	 * query routines: ncmpi_inq tells us how many of each kind of
-	 * "thing" (dimension, variable, attribute) we will find in the
-	 * file  */
+        /* reader knows nothing about dataset, but we can interrogate with
+         * query routines: ncmpi_inq tells us how many of each kind of
+         * "thing" (dimension, variable, attribute) we will find in the
+         * file  */
 
-	ret = ncmpi_inq(ncfile, &ndims, &nvars, &ngatts, &unlimited);
-	if (ret != NC_NOERR) handle_error(ret);
+        ret = ncmpi_inq(ncfile, &ndims, &nvars, &ngatts, &unlimited);
+        if (ret != NC_NOERR) handle_error(ret, __LINE__);
 
-	/* we do not really need the name of the dimension or the variable
-	 * for reading in this example.  we could, in a different example,
-	 * take the name of a variable on the command line and read just
-	 * that one */
+        /* we do not really need the name of the dimension or the variable
+         * for reading in this example.  we could, in a different example,
+         * take the name of a variable on the command line and read just
+         * that one */
 
-	dim_sizes = calloc(ndims, sizeof(MPI_Offset));
-	/* netcdf dimension identifiers are allocated sequentially starting
-	 * at zero; same for variable identifiers */
-	for(i=0; i<ndims; i++)  {
-	    ret = ncmpi_inq_dimlen(ncfile, i, &(dim_sizes[i]) );
-	    if (ret != NC_NOERR) handle_error(ret);
-	}
+        dim_sizes = calloc(ndims, sizeof(MPI_Offset));
+        /* netcdf dimension identifiers are allocated sequentially starting
+         * at zero; same for variable identifiers */
+        for(i=0; i<ndims; i++)  {
+            ret = ncmpi_inq_dimlen(ncfile, i, &(dim_sizes[i]) );
+            if (ret != NC_NOERR) handle_error(ret, __LINE__);
+        }
     }
 
     /* need to inform other MPI processors how many variables we will send */
     MPI_Bcast(&nvars, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     for(i=0; i<nvars; i++) { 
-	/* rank 0 will find out the size of each variable, read it, and
-	 * broadcast it to the rest of the processors */
-	if (rank == 0) {
-	    ret = ncmpi_inq_var(ncfile, i, varname, &type, &var_ndims, dimids,
-		    &var_natts);
-	    if (ret != NC_NOERR) handle_error(ret);
+        /* rank 0 will find out the size of each variable, read it, and
+         * broadcast it to the rest of the processors */
+        if (rank == 0) {
+            ret = ncmpi_inq_var(ncfile, i, varname, &type, &var_ndims, dimids,
+                    &var_natts);
+            if (ret != NC_NOERR) handle_error(ret, __LINE__);
 
-	    for (j=0, var_size=1; j<var_ndims; j++)  {
-		var_size *= dim_sizes[dimids[j]];
-	    }
-	}
-	/* oddity: there's no predefined MPI_Offset type */
-	MPI_Bcast(&var_size, 1, MPI_LONG_DOUBLE, 0, MPI_COMM_WORLD);
+            for (j=0, var_size=1; j<var_ndims; j++)  {
+                var_size *= dim_sizes[dimids[j]];
+            }
+        }
+        /* oddity: there's no predefined MPI_Offset type */
+        MPI_Bcast(&var_size, 1, MPI_LONG_DOUBLE, 0, MPI_COMM_WORLD);
 
-	data = calloc(var_size, sizeof(int));
+        data = calloc(var_size, sizeof(int));
 
-	if (rank == 0) {
-	    switch(type) {
-		case NC_INT:
-		    /* now we have the variable identifiers and we know how big
-		     * they are */
+        if (rank == 0) {
+            switch(type) {
+                case NC_INT:
+                    /* now we have the variable identifiers and we know how big
+                     * they are */
 
-		    /* this approach is not scalable: i/o happens from a single
-		     * processor.  This approach can be ok if the amount of
-		     * data is quite small, but almost always the underlying
-		     * MPI-IO library can do a better job */
+                    /* this approach is not scalable: i/o happens from a single
+                     * processor.  This approach can be ok if the amount of
+                     * data is quite small, but almost always the underlying
+                     * MPI-IO library can do a better job */
 
-		    ret = ncmpi_get_var_int_all(ncfile, j, data);
-		    if (ret != NC_NOERR) handle_error(ret);
-		    break;
-		default:
-		    /* we can do this for all the known netcdf types but this
-		     * example is already getting too long  */
-		    fprintf(stderr, "unsupported NetCDF type \n");
-	    }
-	}
+                    ret = ncmpi_get_var_int_all(ncfile, j, data);
+                    if (ret != NC_NOERR) handle_error(ret, __LINE__);
+                    break;
+                default:
+                    /* we can do this for all the known netcdf types but this
+                     * example is already getting too long  */
+                    fprintf(stderr, "unsupported NetCDF type \n");
+            }
+        }
 
-	/*and finally all processors have the data */
-	MPI_Bcast(data, var_size, MPI_INT, 0, MPI_COMM_WORLD);
-
+        /*and finally all processors have the data */
+        MPI_Bcast(data, var_size, MPI_INT, 0, MPI_COMM_WORLD);
     }
 
     if (rank == 0) {
-	    ret = ncmpi_close(ncfile);
-	    if (ret != NC_NOERR) handle_error(ret);
+        ret = ncmpi_close(ncfile);
+        if (ret != NC_NOERR) handle_error(ret, __LINE__);
     }
 
     MPI_Finalize();
-
     return 0;
 }
 
--- examples/tutorial/pnetcdf-write-from-master.c
+++ examples/tutorial/pnetcdf-write-from-master.c
@@ -8,73 +8,98 @@
 #include <pnetcdf.h>
 #include <stdio.h>
 
-static void handle_error(int status)
+/* This program creates a file, say named output.nc, with the following
+   contents, shown by running ncmpidump command .
+
+    % ncmpidump output.nc 
+    netcdf testfile {
+    // file format: CDF-2 (large file)
+    dimensions:
+            d1 = 4 ;
+    variables:
+            int v1(d1) ;
+        int v2(d1) ;
+
+    // global attributes:
+                :string = "Hello World\n",
+        "" ;
+    data:
+
+         v1 = 0, 1, 2, 3 ;
+
+         v2 = 0, 1, 2, 3 ;
+    }
+*/
+
+static void handle_error(int status, int lineno)
 {
-	fprintf(stderr, "%s\n", ncmpi_strerror(status));
-	exit(-1);
+    fprintf(stderr, "Error at line %d: %s\n", lineno, ncmpi_strerror(status));
+    MPI_Abort(MPI_COMM_WORLD, 1);
 }
 
-
 int main(int argc, char **argv) {
+    int ret, ncfile, nprocs, rank, dimid, varid1, varid2, ndims=1;
+    MPI_Offset start, count=1;
+    char buf[13] = "Hello World\n";
+    int *data;
 
-	int ret, ncfile, nprocs, rank, dimid, varid1, varid2, ndims=1;
-	MPI_Offset start, count=1;
-	char buf[13] = "Hello World\n";
-	int *data;
+    MPI_Init(&argc, &argv);
 
-	MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    if (argc != 2) {
+        if (rank == 0) printf("Usage: %s filename\n", argv[0]);
+        MPI_Finalize();
+        exit(-1);
+    }
 
-	if (rank == 0) {
-	    ret = ncmpi_create(MPI_COMM_SELF, argv[1],
-			    NC_WRITE|NC_64BIT_OFFSET, MPI_INFO_NULL, &ncfile);
-	    if (ret != NC_NOERR) handle_error(ret);
+    if (rank == 0) {
+        ret = ncmpi_create(MPI_COMM_SELF, argv[1],
+                            NC_WRITE|NC_64BIT_OFFSET, MPI_INFO_NULL, &ncfile);
+        if (ret != NC_NOERR) handle_error(ret, __LINE__);
 
-	    ret = ncmpi_def_dim(ncfile, "d1", nprocs, &dimid);
-	    if (ret != NC_NOERR) handle_error(ret);
+        ret = ncmpi_def_dim(ncfile, "d1", nprocs, &dimid);
+        if (ret != NC_NOERR) handle_error(ret, __LINE__);
 
-	    ret = ncmpi_def_var(ncfile, "v1", NC_INT, ndims, &dimid, &varid1);
-	    if (ret != NC_NOERR) handle_error(ret);
+        ret = ncmpi_def_var(ncfile, "v1", NC_INT, ndims, &dimid, &varid1);
+        if (ret != NC_NOERR) handle_error(ret, __LINE__);
 
-	    ret = ncmpi_def_var(ncfile, "v2", NC_INT, ndims, &dimid, &varid2);
-	    if (ret != NC_NOERR) handle_error(ret);
+        ret = ncmpi_def_var(ncfile, "v2", NC_INT, ndims, &dimid, &varid2);
+        if (ret != NC_NOERR) handle_error(ret, __LINE__);
 
-	    ret = ncmpi_put_att_text(ncfile, NC_GLOBAL, "string", 13, buf);
-	    if (ret != NC_NOERR) handle_error(ret);
-	
-	    ret = ncmpi_enddef(ncfile);
-	    if (ret != NC_NOERR) handle_error(ret);
+        ret = ncmpi_put_att_text(ncfile, NC_GLOBAL, "string", 13, buf);
+        if (ret != NC_NOERR) handle_error(ret, __LINE__);
+        
+        ret = ncmpi_enddef(ncfile);
+        if (ret != NC_NOERR) handle_error(ret, __LINE__);
 
-	    /* first reason this approach is not scalable:  need to allocate
-	     * enough memory to hold data from all processors */
-	    data = calloc(nprocs, sizeof(int));
-	}
+        /* first reason this approach is not scalable:  need to allocate
+        * enough memory to hold data from all processors */
+        data = calloc(nprocs, sizeof(int));
+    }
 
-	/* second reason this approch is not scalable: sending to rank 0
-	 * introduces a serialization point, even if using an optimized
-	 * collective routine */
-	MPI_Gather(&rank, 1, MPI_INT, 
-			data, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    /* second reason this approch is not scalable: sending to rank 0
+     * introduces a serialization point, even if using an optimized
+     * collective routine */
+    MPI_Gather(&rank, 1, MPI_INT, data, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-	if (rank == 0) {
-	    /* and lastly, the third reason this approach is not scalable: I/O
-	     * happens from a single processor.  This approach can be ok if the
-	     * amount of data is quite small, but almost always the underlying
-	     * MPI-IO library can do a better job */
-	    start=0, count=nprocs;
-	    ret = ncmpi_put_vara_int_all(ncfile, varid1, &start, &count, data);
-	    if (ret != NC_NOERR) handle_error(ret);
+    if (rank == 0) {
+        /* and lastly, the third reason this approach is not scalable: I/O
+         * happens from a single processor.  This approach can be ok if the
+         * amount of data is quite small, but almost always the underlying
+         * MPI-IO library can do a better job */
+        start=0, count=nprocs;
+        ret = ncmpi_put_vara_int_all(ncfile, varid1, &start, &count, data);
+        if (ret != NC_NOERR) handle_error(ret, __LINE__);
 
-	    ret = ncmpi_put_vara_int_all(ncfile, varid2, &start, &count, data);
-	    if (ret != NC_NOERR) handle_error(ret);
+        ret = ncmpi_put_vara_int_all(ncfile, varid2, &start, &count, data);
+        if (ret != NC_NOERR) handle_error(ret, __LINE__);
 
-	    ret = ncmpi_close(ncfile);
-	    if (ret != NC_NOERR) handle_error(ret);
-	}
+        ret = ncmpi_close(ncfile);
+        if (ret != NC_NOERR) handle_error(ret, __LINE__);
+    }
 
-	MPI_Finalize();
-
-	return 0;
+    MPI_Finalize();
+    return 0;
 }
