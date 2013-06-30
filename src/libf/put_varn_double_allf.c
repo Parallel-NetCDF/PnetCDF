bb6d6837599465bb2f50a17e9b8b94eafc64772c
Wei-keng Liao <wkliao@users.noreply.github.com>
2013-06-30 23:12:58 +0000

    Fortran part: remove varnx; add varn
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@1333 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- src/libf/put_varn_double_allf.c
+++ src/libf/put_varn_double_allf.c
@@ -21,9 +21,51 @@
 
 /* Prototypes for the Fortran interfaces */
 #include "mpifnetcdf.h"
-FORTRAN_API int FORT_CALL nfmpi_put_varn_double_all_ ( int *v1, int *v2, int *v3, double *v4 ){
+FORTRAN_API int FORT_CALL nfmpi_put_varn_double_all_ ( int *v1, int *v2, int *v3, MPI_Offset *v4, MPI_Offset *v5, double *v6 ){
     int ierr;
     int l2 = *v2 - 1;
-    ierr = ncmpi_put_varn_double_all( *v1, l2, *v3, v4 );
+    MPI_Offset **l4 = 0;
+    MPI_Offset **l5 = 0;
+
+    { int ndims = ncmpixVardim(*v1,*v2-1); /* var's number dims */
+    if (ndims > 0) {
+        int li, lj;
+        l4    = (MPI_Offset**)malloc(*v3 * sizeof(MPI_Offset*) );
+        l4[0] = (MPI_Offset*) malloc(*v3 * ndims * sizeof(MPI_Offset));
+        for (lj=1; lj<*v3; lj++) 
+            l4[lj] = l4[lj-1] + ndims;
+        for (lj=0; lj<*v3; lj++) 
+            for (li=0; li<ndims; li++) 
+                l4[lj][li] = v4[lj*ndims + ndims-1-li] - 1;
+    }
+    else if (ndims < 0) {
+        /* Error return */
+        ierr = ndims; 
+	return ierr;
+    }
+    }
+
+    { int ndims = ncmpixVardim(*v1,*v2-1); /* var's number dims */
+    if (ndims > 0) {
+        int li, lj;
+        l5    = (MPI_Offset**)malloc(*v3 * sizeof(MPI_Offset*) );
+        l5[0] = (MPI_Offset*) malloc(*v3 * ndims * sizeof(MPI_Offset));
+        for (lj=1; lj<*v3; lj++) 
+            l5[lj] = l5[lj-1] + ndims;
+        for (lj=0; lj<*v3; lj++) 
+            for (li=0; li<ndims; li++) 
+                l5[lj][li] = v5[lj*ndims + ndims-1-li];
+    }
+    else if (ndims < 0) {
+        /* Error return */
+        ierr = ndims; 
+	return ierr;
+    }
+    }
+    ierr = ncmpi_put_varn_double_all( *v1, l2, *v3, l4, l5, v6 );
+
+    if (l4) { free(l4[0]); free(l4); }
+
+    if (l5) { free(l5[0]); free(l5); }
     return ierr;
 }
