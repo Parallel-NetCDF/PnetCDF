63132642fc235a762f0783810a2b27f8a1bf6b1d
Wei-keng Liao <wkliao@users.noreply.github.com>
2013-12-16 00:20:29 +0000

    add test for Fortran fixed form
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@1531 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

---
+++ test/fandc/fixedform.f90
@@ -0,0 +1,22 @@
+!
+!   Copyright (C) 2013, Northwestern University and Argonne National Laboratory
+!   See COPYRIGHT notice in top-level directory.
+!
+! $Id$
+
+!
+! This program tests whether header file pnetcdf.inc conforms Fortran fixed form
+!
+
+      program main
+        implicit none
+        include "mpif.h"
+        include "pnetcdf.inc"
+
+        character(LEN=128) filename
+        integer ncid, err
+
+        err = nfmpi_create(MPI_COMM_WORLD, filename, IOR(NF_CLOBBER,    &
+     &                     NF_64BIT_DATA), MPI_INFO_NULL, ncid)
+      end program main
+
