160a98a4f230b380552322950e7ce0765f80c2c4
Wei-keng Liao <wkliao@users.noreply.github.com>
2014-04-22 01:14:45 +0000

    update test program record.c
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@1593 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- test/testcases/record.c
+++ test/testcases/record.c
@@ -15,7 +15,8 @@
  * This program tests if the number of records is updated correctly. It first
  * writes to the 2nd record of a variable, followed by writing to the 1st
  * record. After the 2nd write, a call to ncmpi_inq_dimlen() or ncmpi_inq_dim()
- * should report seeing 2 records.
+ * should report seeing 2 records. Then, it does a similar test under
+ * independent data mode.
  *
  * The compile and run commands are given below.
  *
@@ -67,6 +68,25 @@ int main(int argc, char** argv) {
     if (length != 2) {
         printf("Error: expecting 2 records, but got %lld record(s)\n",length);
         pass = 0;
+    }
+
+    if (pass) { /* test independent data mode */
+        err = ncmpi_begin_indep_data(ncid); ERR
+        /* write the 4th record */
+        start = 3; count = 1;
+        err = ncmpi_put_vara_int(ncid, varid, &start, &count, &buf); ERR
+
+        /* write the 3rd record */
+        start = 2; count = 1;
+        err = ncmpi_put_vara_int(ncid, varid, &start, &count, &buf); ERR
+
+        err = ncmpi_inq_dimlen(ncid, dimid, &length); ERR
+        if (length != 4) {
+            printf("Error: expecting 4 records, but got %lld record(s)\n",
+                   length);
+            pass = 0;
+        }
+        err = ncmpi_end_indep_data(ncid); ERR
     }
 
     err = ncmpi_close(ncid); ERR
