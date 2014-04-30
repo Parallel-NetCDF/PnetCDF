023e80f0dcd5c39e7ff8060a4b62e3e1881892e2
Wei-keng Liao <wkliao@users.noreply.github.com>
2014-04-30 16:25:43 +0000

    add attributes to a C example program
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@1598 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- examples/C/put_vara.c
+++ examples/C/put_vara.c
@@ -8,6 +8,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> /* strcpy() */
+#include <time.h>   /* time() localtime(), asctime() */
 #include <assert.h>
 #include <mpi.h>
 #include <pnetcdf.h>
@@ -34,12 +36,17 @@
  *
  *    % ncmpidump /pvfs2/wkliao/testfile.nc
  *    netcdf testfile {
- *    // file format: CDF-5 (big variables)
+ *    // file format: CDF-1
  *    dimensions:
  *            y = 10 ;
  *            x = 16 ;
  *    variables:
  *            int var(y, x) ;
+ *                var:str_att_name = "example attribute of type text." ;
+ *                var:float_att_name = 0.f, 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f ;
+ *    // global attributes:
+ *                :history = "Wed Apr 30 11:18:58 2014\n",
+ *       "" ;
  *    data:
  *
  *     var =
@@ -63,6 +70,8 @@ int main(int argc, char** argv) {
     char *filename="testfile.nc";
     int i, j, rank, nprocs, err;
     int ncid, cmode, varid, dimid[2], buf[NY][NX];
+    char str_att[128];
+    float float_att[100];
     MPI_Offset  global_ny, global_nx;
     MPI_Offset start[2], count[2];
 
@@ -78,7 +87,7 @@ int main(int argc, char** argv) {
     if (argc == 2) filename = argv[1];
 
     /* create a new file for writing ----------------------------------------*/
-    cmode = NC_CLOBBER | NC_64BIT_DATA;
+    cmode = NC_CLOBBER;
     err = ncmpi_create(MPI_COMM_WORLD, filename, cmode, MPI_INFO_NULL, &ncid);
     ERR
 
@@ -90,6 +99,17 @@ int main(int argc, char** argv) {
         for (j=0; j<NX; j++)
              buf[i][j] = rank;
 
+    /* add a global attribute: a time stamp at rank 0 */
+    time_t ltime = time(NULL); /* get the current calendar time */
+    asctime_r(localtime(&ltime), str_att);
+
+    /* make sure the time string are consistent among all processes */
+    MPI_Bcast(str_att, strlen(str_att), MPI_CHAR, 0, MPI_COMM_WORLD);
+
+    err = ncmpi_put_att_text(ncid, NC_GLOBAL, "history", strlen(str_att),
+                             &str_att[0]);
+    ERR
+
     /* define dimensions x and y */
     err = ncmpi_def_dim(ncid, "Y", global_ny, &dimid[0]);
     ERR
@@ -99,6 +119,18 @@ int main(int argc, char** argv) {
     /* define a 2D variable of integer type */
     err = ncmpi_def_var(ncid, "var", NC_INT, 2, dimid, &varid);
     ERR
+
+    /* add attributes to the variable */
+    strcpy(str_att, "example attribute of type text.");
+    err = ncmpi_put_att_text(ncid, varid, "str_att_name", strlen(str_att),
+                             &str_att[0]);
+    ERR
+
+    for (i=0; i<8; i++) float_att[i] = i;
+    err = ncmpi_put_att_float(ncid, varid, "float_att_name", NC_FLOAT, 8,
+                              &float_att[0]);
+    ERR
+
     /* do not forget to exit define mode */
     err = ncmpi_enddef(ncid);
     ERR
