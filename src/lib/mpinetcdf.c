4e4bcbf2edd12530faacc5631bce812b09d8b9b8
Wei-keng Liao <wkliao@users.noreply.github.com>
2014-12-03 04:40:46 +0000

    revert r1895 and add note on how pnetcdf_libvers and ncmpi_inq_libvers() are used for different purposes
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@1903 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- src/lib/mpinetcdf.c
+++ src/lib/mpinetcdf.c
@@ -37,13 +37,25 @@ static int ncmpii_end_indep_data(NC *ncp);
 static const char pnetcdf_libvers[] =
         "\044Id: \100(#) PnetCDF library version "PNETCDF_VERSION" of "PNETCDF_RELEASE_DATE" $";
 
+/* pnetcdf_libvers is slightly different from the one returned from
+ * ncmpi_inq_libvers(). The string pnetcdf_libvers is for command "ident" to
+ * use. People can run command ident libpnetcdf.a to obtain the version of a
+ * library (or an executable built from that library). In PnetCDF case, the
+ * command will print the string of pnetcdf_libvers. Command "ident' looks for
+ * a specific keyword pattern and print it. See man page of ident.
+ *
+ * The API ncmpi_inq_libvers() below on the other hand returns a string to be
+ * used by the utility tools like ncmpidump, ncmpigen, etc. Check the last line
+ * of output from command "ncmpidump -v".
+ */
+
 /*----< ncmpi_inq_libvers() >------------------------------------------------*/
 inline const char*
 ncmpi_inq_libvers(void) {
 
     /* match the style used by netCDF API nc_inq_libvers()
      * for example, "4.3.0 of Jun 16 2013 12:11:30 $" */
-    return pnetcdf_libvers;
+    return PNETCDF_VERSION " of " PNETCDF_RELEASE_DATE;
 }
 
 /* Begin Of Dataset Functions */
