2e1132194e544a360048b3d1be8b0d2604719c65
Wei-keng Liao <wkliao@users.noreply.github.com>
2015-09-26 04:16:02 +0000

    add checking for file extension f77/F77
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@2132 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- test/common/testutilsf.F90
+++ test/common/testutilsf.F90
@@ -6,48 +6,48 @@
 !
 !     $Id$
 
-    ! This function gets the executable name and output file name from the
-    ! command line.
-    integer function get_args(cmd, filename)
+      ! This function gets the executable name and output file name from the
+      ! command line.
+      integer function get_args(cmd, filename)
 #ifdef NAGf90Fortran
-        USE F90_UNIX_ENV, only : iargc, getarg
-        implicit none
+          USE F90_UNIX_ENV, only : iargc, getarg
+          implicit none
 #else
-        implicit none
-        integer iargc
+          implicit none
+          integer iargc
 #endif
-        integer argc
-        character(len=*) cmd, filename
+          integer argc
+          character(len=*) cmd, filename
 
-        get_args = 1
-        call getarg(0, cmd)
-        argc = IARGC()
-        if (argc .GT. 1) then
-           print*,'Usage: ',trim(cmd),' [filename]'
-           get_args = 0
-           return
-        endif
-        if (argc .EQ. 1) call getarg(1, filename)
-    end function get_args
+          get_args = 1
+          call getarg(0, cmd)
+          argc = IARGC()
+          if (argc .GT. 1) then
+              print*,'Usage: ',trim(cmd),' [filename]'
+              get_args = 0
+              return
+          endif
+          if (argc .EQ. 1) call getarg(1, filename)
+      end function get_args
 
-    ! This function prints the pass/fail message on screen
-    subroutine pass_fail(nerrs, msg)
-        implicit none
-        integer nerrs
-        character(len=*) msg
+      ! This function prints the pass/fail message on screen
+      subroutine pass_fail(nerrs, msg)
+          implicit none
+          integer nerrs
+          character(len=*) msg
 
-        ! local variables
-        CHARACTER ESC
-        PARAMETER (ESC=char(27))
+          ! local variables
+          CHARACTER ESC
+          PARAMETER (ESC=char(27))
 
-        CHARACTER (LEN=20) PASS_STR, FAIL_STR
-        PARAMETER (PASS_STR='------ '//ESC//'[32mpass'//ESC//'[0m')
-        PARAMETER (FAIL_STR='------ '//ESC//'[31mfail'//ESC//'[0m')
+          CHARACTER (LEN=20) PASS_STR, FAIL_STR
+          PARAMETER (PASS_STR='------ '//ESC//'[32mpass'//ESC//'[0m')
+          PARAMETER (FAIL_STR='------ '//ESC//'[31mfail'//ESC//'[0m')
 
-        if (nerrs .EQ. 0) then
-            write(*,"(A67,A)") msg, PASS_STR
-        else
-            write(*,"(A67,A)") msg, FAIL_STR
-        endif
-    end subroutine pass_fail
+          if (nerrs .EQ. 0) then
+              write(*,"(A67,A)") msg, PASS_STR
+          else
+              write(*,"(A67,A)") msg, FAIL_STR
+          endif
+      end subroutine pass_fail
 
