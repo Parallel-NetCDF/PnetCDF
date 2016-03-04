be8feac31484e35c0202de7609d3a212b0dc06ab
Rob Latham <rlatham@gmail.com>
2016-03-04 16:18:32 +0000

    A first start at a jenkins script
    It's easier to modify this test script than it is to log into the jenkins
    server and tweak the build rules.  Now we just tell jenkins to check out
    pnetcdf and run this script
    
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@2344 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

---
+++ test/jenkins.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# MCS hosts a jenkins server (a continuous-integration build bot) at
+# https://jenkins-ci.mcs.anl.gov/job/Parallel-NetCDF/
+
+# this is just a beginning.
+# - Would like to integrate coverity into the reports
+# - our tests should report something Jenkins understands so that we get back
+#   a finer-grained "N of M tests failed" instead of a single "pass/fail" for
+#   all tests
+
+# presumes the presence of the autotools and an mpi compiler.  The jenkins
+# build slaves use the MCS workstation environment, which today is
+# autoconf-2.68 and mpich-1.4.1
+autoreconf -fi
+
+# We are using the following Jenkins environment variables:
+# WORKSPACE
+#     The absolute path of the directory assigned to the build as a workspace.
+# BUILD_TAG
+#    String of "jenkins-${JOB_NAME}-${BUILD_NUMBER}". Convenient to put into a
+#    resource file, a jar file, etc for easier identification.
+./configure --prefix=${WORKSPACE:-`pwd`}/install-${BUILD_TAG:-`date +"Y%m%d-%H%M%S"`}
+make
+make testing
+make ptest
