d9085e31f8d1f8c8460613f196dce51ff6240d72
Wei-keng Liao <wkliao@users.noreply.github.com>
2016-06-09 18:00:47 +0000

    add more test for put vars case
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@2452 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- test/testcases/ncmpi_vars_null_stride.c
+++ test/testcases/ncmpi_vars_null_stride.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>  /* memset() */
 #include <mpi.h>
 #include <pnetcdf.h>
 
@@ -18,14 +19,27 @@
     goto fn_exit; \
 }
 
-#define NDIMS 1
+/* check if user put buffer contents altered */
+#define CHECK_PUT_BUF \
+    for (i=0; i<NY*NX; i++) { \
+        if (buf[i] != rank+10) { \
+            printf("Error at line %d: user put buffer[%d] altered from %d to %d\n", \
+                   __LINE__, i, rank+10, buf[i]); \
+            nerrs++; \
+        } \
+    }
+
+#define NDIMS 2
+#define NY 4
+#define NX 2
+
 int main(int argc, char **argv)
 {
-    int err, nerrs=0, ncfile, dimid, varid, ndims=NDIMS;
-    int i, nprocs, rank;
+    int err, nerrs=0, ncid, dimid[NDIMS], varid[5], ndims=NDIMS;
+    int i, j, k, nprocs, rank, req, *buf;
     MPI_Offset start[NDIMS] = {0};
     MPI_Offset count[NDIMS] = {0};
-    int buf[512];
+    MPI_Offset stride[NDIMS] = {0};
     char *filename="testfile.nc";
 
     MPI_Init(&argc, &argv);
@@ -45,35 +59,207 @@ int main(int argc, char **argv)
         printf("%-66s ------ ", cmd_str); fflush(stdout);
     }
 
-    err = ncmpi_create(MPI_COMM_WORLD, filename, 0, MPI_INFO_NULL, &ncfile);
+    err = ncmpi_create(MPI_COMM_WORLD, filename, 0, MPI_INFO_NULL, &ncid);
     if (err != NC_NOERR) HANDLE_ERROR(err)
 
-    err = ncmpi_def_dim(ncfile, "d1", nprocs, &dimid);
+    err = ncmpi_def_dim(ncid, "Y", NY, &dimid[0]);
     if (err != NC_NOERR) HANDLE_ERROR(err)
 
-    err = ncmpi_def_var(ncfile, "v1", NC_INT, ndims, &dimid, &varid);
+    err = ncmpi_def_dim(ncid, "X", nprocs*NX, &dimid[1]);
     if (err != NC_NOERR) HANDLE_ERROR(err)
 
-    err = ncmpi_enddef(ncfile);
+    err = ncmpi_def_var(ncid, "v0", NC_INT, ndims, dimid, &varid[0]);
     if (err != NC_NOERR) HANDLE_ERROR(err)
 
-    start[0] = rank;
-    count[0] = 1;
-    for (i=0; i<512; i++) buf[i] = rank+10;
-    err = ncmpi_put_vars_int_all(ncfile, varid, start, count, NULL, buf);
+    err = ncmpi_def_var(ncid, "v1", NC_INT, ndims, dimid, &varid[1]);
     if (err != NC_NOERR) HANDLE_ERROR(err)
 
-    /* check if user put buffer contents altered */
-    for (i=0; i<512; i++) {
-        if (buf[i] != rank+10) {
-            printf("Error: user put buffer[%d] altered from %d to %d\n",
-                   i, rank+10, buf[i]);
-            nerrs++;
+    err = ncmpi_def_var(ncid, "v2", NC_INT, ndims, dimid, &varid[2]);
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+
+    err = ncmpi_def_var(ncid, "v3", NC_INT, ndims, dimid, &varid[3]);
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+
+    err = ncmpi_def_var(ncid, "v4", NC_INT, ndims, dimid, &varid[4]);
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+
+    err = ncmpi_enddef(ncid);
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+
+    start[0] = 0;
+    start[1] = rank*NX;
+    count[0] = NY;
+    count[1] = NX;
+    buf = (int*) malloc((size_t)NY * NX * sizeof(int));
+    for (i=0; i<NY*NX; i++) buf[i] = rank+10;
+
+    err = ncmpi_put_vara_int_all(ncid, varid[0], start, count, buf);
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+    CHECK_PUT_BUF
+
+    err = ncmpi_put_vars_int_all(ncid, varid[1], start, count, NULL, buf);
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+    CHECK_PUT_BUF
+
+    start[0] = 0;
+    start[1] = rank;
+    count[0] = NY;
+    count[1] = NX;
+    stride[0] = 1;
+    stride[1] = nprocs;
+    err = ncmpi_put_vars_int_all(ncid, varid[2], start, count, stride, buf);
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+    CHECK_PUT_BUF
+
+    /* test bput_vars */
+    err = ncmpi_buffer_attach(ncid, NY*NX*sizeof(int));
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+
+    start[0] = 0;
+    start[1] = rank*NX;
+    count[0] = NY;
+    count[1] = NX;
+    err = ncmpi_bput_vars_int(ncid, varid[3], start, count, NULL, buf, &req);
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+
+    err = ncmpi_wait_all(ncid, 1, &req, NULL);
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+    CHECK_PUT_BUF
+
+    start[0] = 0;
+    start[1] = rank;
+    count[0] = NY;
+    count[1] = NX;
+    stride[0] = 1;
+    stride[1] = nprocs;
+    err = ncmpi_bput_vars_int(ncid, varid[4], start, count, stride, buf, &req);
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+
+    err = ncmpi_wait_all(ncid, 1, &req, NULL);
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+    CHECK_PUT_BUF
+    free(buf);
+
+    buf = (int*) malloc((size_t)NY * NX * nprocs * sizeof(int));
+    memset(buf, 0, (size_t)NY * NX * nprocs * sizeof(int));
+    err = ncmpi_get_var_int_all(ncid, varid[0], buf);
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+
+    /* check read buffer contents */
+    /*  v0 =
+     *    10, 10, 11, 11, 12, 12, 13, 13,
+     *    10, 10, 11, 11, 12, 12, 13, 13,
+     *    10, 10, 11, 11, 12, 12, 13, 13,
+     *    10, 10, 11, 11, 12, 12, 13, 13 ;
+     */
+    for (i=0; i<NY; i++) {
+        for (j=0; j<nprocs; j++) {
+            for (k=0; k<NX; k++) {
+                if (buf[i*nprocs*NX+j*NX+k] != j+10) {
+                    printf("Error at line %d: expected buffer[%d]=%d but got %d\n",
+                           __LINE__,i*nprocs*NX+j*NX+k, j+10, buf[i*nprocs*NX+j*NX+k]);
+                    nerrs++;
+                }
+            }
         }
     }
 
-    err = ncmpi_close(ncfile);
+    memset(buf, 0, (size_t)NY * NX * nprocs * sizeof(int));
+    err = ncmpi_get_var_int_all(ncid, varid[1], buf);
     if (err != NC_NOERR) HANDLE_ERROR(err)
+
+    /* check read buffer contents */
+    /*  v1 =
+     *    10, 10, 11, 11, 12, 12, 13, 13,
+     *    10, 10, 11, 11, 12, 12, 13, 13,
+     *    10, 10, 11, 11, 12, 12, 13, 13,
+     *    10, 10, 11, 11, 12, 12, 13, 13 ;
+     */
+    for (i=0; i<NY; i++) {
+        for (j=0; j<nprocs; j++) {
+            for (k=0; k<NX; k++) {
+                if (buf[i*nprocs*NX+j*NX+k] != j+10) {
+                    printf("Error at line %d: expected buffer[%d]=%d but got %d\n",
+                           __LINE__,i*nprocs*NX+j*NX+k, j+10, buf[i*nprocs*NX+j*NX+k]);
+                    nerrs++;
+                }
+            }
+        }
+    }
+
+    memset(buf, 0, (size_t)NY * NX * nprocs * sizeof(int));
+    err = ncmpi_get_var_int_all(ncid, varid[2], buf);
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+
+    /* check read buffer contents */
+    /*  v2 =
+     *    10, 11, 12, 13, 10, 11, 12, 13,
+     *    10, 11, 12, 13, 10, 11, 12, 13,
+     *    10, 11, 12, 13, 10, 11, 12, 13,
+     *    10, 11, 12, 13, 10, 11, 12, 13 ;
+     */
+    for (i=0; i<NY; i++) {
+        for (k=0; k<NX; k++) {
+            for (j=0; j<nprocs; j++) {
+                if (buf[i*nprocs*NX+k*nprocs+j] != j+10) {
+                    printf("Error at line %d: expected buffer[%d]=%d but got %d\n",
+                           __LINE__,i*nprocs*NX+k*nprocs+j, j+10, buf[i*nprocs*NX+k*nprocs+j]);
+                    nerrs++;
+                }
+            }
+        }
+    }
+
+    memset(buf, 0, (size_t)NY * NX * nprocs * sizeof(int));
+    err = ncmpi_get_var_int_all(ncid, varid[3], buf);
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+
+    /* check read buffer contents */
+    /*  v3 =
+     *    10, 10, 11, 11, 12, 12, 13, 13,
+     *    10, 10, 11, 11, 12, 12, 13, 13,
+     *    10, 10, 11, 11, 12, 12, 13, 13,
+     *    10, 10, 11, 11, 12, 12, 13, 13 ;
+     */
+    for (i=0; i<NY; i++) {
+        for (j=0; j<nprocs; j++) {
+            for (k=0; k<NX; k++) {
+                if (buf[i*nprocs*NX+j*NX+k] != j+10) {
+                    printf("Error at line %d: expected buffer[%d]=%d but got %d\n",
+                           __LINE__,i*nprocs*NX+j*NX+k, j+10, buf[i*nprocs*NX+j*NX+k]);
+                    nerrs++;
+                }
+            }
+        }
+    }
+
+    memset(buf, 0, (size_t)NY * NX * nprocs * sizeof(int));
+    err = ncmpi_get_var_int_all(ncid, varid[4], buf);
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+
+    /* check read buffer contents */
+    /*  v4 =
+     *    10, 11, 12, 13, 10, 11, 12, 13,
+     *    10, 11, 12, 13, 10, 11, 12, 13,
+     *    10, 11, 12, 13, 10, 11, 12, 13,
+     *    10, 11, 12, 13, 10, 11, 12, 13 ;
+     */
+    for (i=0; i<NY; i++) {
+        for (k=0; k<NX; k++) {
+            for (j=0; j<nprocs; j++) {
+                if (buf[i*nprocs*NX+k*nprocs+j] != j+10) {
+                    printf("Error at line %d: expected buffer[%d]=%d but got %d\n",
+                           __LINE__,i*nprocs*NX+k*nprocs+j, j+10, buf[i*nprocs*NX+k*nprocs+j]);
+                    nerrs++;
+                }
+            }
+        }
+    }
+
+    err = ncmpi_close(ncid);
+    if (err != NC_NOERR) HANDLE_ERROR(err)
+
+    free(buf);
 
     /* check if PnetCDF freed all internal malloc */
     MPI_Offset malloc_size, sum_size;
