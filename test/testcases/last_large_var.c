b53aeb99cb88e6e33fdcf3cb5d22f131b0857e12
Wei-keng Liao <wkliao@users.noreply.github.com>
2016-06-23 19:14:10 +0000

    add more tests for exception of defining larger-than-4GB variables, record and fixed-size
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@2454 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- test/testcases/last_large_var.c
+++ test/testcases/last_large_var.c
@@ -65,7 +65,7 @@ int check_last_var(char *filename)
     err = ncmpi_def_dim(ncid, "YY", 66661, &dimid[2]); ERR
     err = ncmpi_def_dim(ncid, "XX", 66661, &dimid[3]); ERR
 
-    /* define only fixed-size variables */
+    /* define only fixed-size variables and the last one is "big" */
     err = ncmpi_def_var(ncid, "var", NC_INT, 1, dimid+1, &varid); ERR
     err = ncmpi_def_var(ncid, "var_last", NC_FLOAT, 2, dimid+2, &varid); ERR
 
@@ -76,7 +76,54 @@ int check_last_var(char *filename)
 }
 
 static
-int check_rec_var(char *filename)
+int check_fix_var(char *filename)
+{
+    int err, nerrs=0, ncid, cmode, varid, dimid[4];
+
+    /* create a new CDF-1 file ----------------------------------------------*/
+    cmode = NC_CLOBBER;
+    err = ncmpi_create(MPI_COMM_WORLD, filename, cmode, MPI_INFO_NULL, &ncid);
+    ERR
+
+    err = ncmpi_def_dim(ncid, "X", 536870911, &dimid[0]); ERR
+
+    /* define only fixed-size variables and no one is "big"
+     * make the starting offset of last one > 2GiB (illegal for CDF-1)
+     */
+    err = ncmpi_def_var(ncid, "var1", NC_INT,   1, dimid, &varid); ERR
+    err = ncmpi_def_var(ncid, "var2", NC_FLOAT, 1, dimid, &varid); ERR
+    err = ncmpi_def_var(ncid, "var3", NC_SHORT, 1, dimid, &varid); ERR
+    err = ncmpi_def_var(ncid, "var4", NC_INT,   1, dimid, &varid); ERR
+
+    err = ncmpi_close(ncid);
+    if (err != NC_EVARSIZE) {
+        printf("\nError at line=%d: expecting error code NC_EVARSIZE but got %s\n",__LINE__,nc_err_code_name(err));
+        nerrs++;
+    }
+
+    /* create a new CDF-2 file ----------------------------------------------*/
+    cmode = NC_CLOBBER | NC_64BIT_OFFSET;
+    err = ncmpi_create(MPI_COMM_WORLD, filename, cmode, MPI_INFO_NULL, &ncid);
+    ERR
+
+    err = ncmpi_def_dim(ncid, "X", 536870911, &dimid[0]); ERR
+
+    /* define only fixed-size variables and no one is "big"
+     * make the starting offset of last one > 2GiB (legal for CDF-2)
+     */
+    err = ncmpi_def_var(ncid, "var1", NC_INT,   1, dimid, &varid); ERR
+    err = ncmpi_def_var(ncid, "var2", NC_FLOAT, 1, dimid, &varid); ERR
+    err = ncmpi_def_var(ncid, "var3", NC_SHORT, 1, dimid, &varid); ERR
+    err = ncmpi_def_var(ncid, "var4", NC_INT,   1, dimid, &varid); ERR
+
+    err = ncmpi_enddef(ncid); ERR
+    err = ncmpi_close(ncid); ERR
+
+    return nerrs;
+}
+
+static
+int check_fix_rec_var(char *filename)
 {
     int err, nerrs=0, ncid, cmode, varid, dimid[4];
 
@@ -108,6 +155,77 @@ int check_rec_var(char *filename)
     return nerrs;
 }
 
+/* http://www.unidata.ucar.edu/software/netcdf/docs/file_structure_and_performance.html#classic_format_limitations
+ * If you use the unlimited dimension, record variables may exceed 2 GiB in
+ * size, as long as the offset of the start of each record variable within a
+ * record is less than 2 GiB - 4.
+ */
+static
+int check_rec_var(char *filename, int cmode)
+{
+    int err, nerrs=0, ncid, varid, dimid[3];
+
+    /* create a new file ---------------------------------------------------*/
+    cmode |= NC_CLOBBER;
+    err = ncmpi_create(MPI_COMM_WORLD, filename, cmode, MPI_INFO_NULL, &ncid);
+    ERR
+
+    err = ncmpi_def_dim(ncid, "Z", NC_UNLIMITED, &dimid[0]); ERR
+    err = ncmpi_def_dim(ncid, "Y", 66661,        &dimid[1]); ERR
+    err = ncmpi_def_dim(ncid, "X", 66661,        &dimid[2]); ERR
+
+    /* define record variables: last one is large */
+    err = ncmpi_def_var(ncid, "var",       NC_INT,   1, dimid, &varid); ERR
+    err = ncmpi_def_var(ncid, "var_large", NC_FLOAT, 3, dimid, &varid); ERR
+
+    err = ncmpi_close(ncid); ERR
+
+    /* create a new file ---------------------------------------------------*/
+    cmode |= NC_CLOBBER;
+    err = ncmpi_create(MPI_COMM_WORLD, filename, cmode, MPI_INFO_NULL, &ncid);
+    ERR
+
+    err = ncmpi_def_dim(ncid, "Z", NC_UNLIMITED, &dimid[0]); ERR
+    err = ncmpi_def_dim(ncid, "Y", 1048576, &dimid[1]); ERR
+    err = ncmpi_def_dim(ncid, "X", 1000, &dimid[2]); ERR
+
+    /* define record variables: both starting offsets are < 2^31-4 */
+    err = ncmpi_def_var(ncid, "var1", NC_SHORT, 3, dimid, &varid); ERR
+    err = ncmpi_def_var(ncid, "var2", NC_SHORT, 3, dimid, &varid); ERR
+
+    err = ncmpi_close(ncid); ERR
+
+    /* create a new file ---------------------------------------------------*/
+    cmode |= NC_CLOBBER;
+    err = ncmpi_create(MPI_COMM_WORLD, filename, cmode, MPI_INFO_NULL, &ncid);
+    ERR
+
+    err = ncmpi_def_dim(ncid, "Z", NC_UNLIMITED, &dimid[0]); ERR
+    err = ncmpi_def_dim(ncid, "Y", 1048576, &dimid[1]); ERR
+    err = ncmpi_def_dim(ncid, "X", 1024, &dimid[2]); ERR
+
+    /* define record variables: some starting offsets are > 2^31-4 */
+    err = ncmpi_def_var(ncid, "var1", NC_SHORT, 3, dimid, &varid); ERR
+    err = ncmpi_def_var(ncid, "var2", NC_SHORT, 3, dimid, &varid); ERR
+    err = ncmpi_def_var(ncid, "var3", NC_SHORT, 3, dimid, &varid); ERR
+    err = ncmpi_def_var(ncid, "var4", NC_SHORT, 3, dimid, &varid); ERR
+
+    err = ncmpi_close(ncid);
+    if (cmode & NC_64BIT_OFFSET || cmode & NC_64BIT_DATA) ERR
+    else if (err != NC_EVARSIZE) {
+        printf("\nError at line=%d: expecting error code NC_EVARSIZE but got %s\n",__LINE__,nc_err_code_name(err));
+        nerrs++;
+    }
+
+    return nerrs;
+}
+
+/* http://www.unidata.ucar.edu/software/netcdf/docs/file_structure_and_performance.html#classic_format_limitations
+ * If you don't use the unlimited dimension, only one variable can exceed 2 GiB
+ * in size, but it can be as large as the underlying file system permits. It
+ * must be the last variable in the dataset, and the offset to the beginning of
+ * this variable must be less than about 2 GiB.
+ */
 static
 int check_not_last_var(char *filename)
 {
@@ -124,8 +242,8 @@ int check_not_last_var(char *filename)
     err = ncmpi_def_dim(ncid, "XX", 66661, &dimid[3]); ERR
 
     /* the large variable is not the last */
-    err = ncmpi_def_var(ncid, "var_last", NC_FLOAT, 2, dimid+2, &varid); ERR
-    err = ncmpi_def_var(ncid, "var", NC_INT, 1, dimid+1, &varid); ERR
+    err = ncmpi_def_var(ncid, "var_large", NC_FLOAT, 2, dimid+2, &varid); ERR
+    err = ncmpi_def_var(ncid, "var",       NC_INT,   1, dimid+1, &varid); ERR
 
     err = ncmpi_enddef(ncid);
     if (err != NC_EVARSIZE) {
@@ -232,8 +350,12 @@ int main(int argc, char** argv)
         printf("%-66s ------ ", cmd_str); fflush(stdout);
     }
 
+    nerrs += check_fix_var(filename);
     nerrs += check_last_var(filename);
-    nerrs += check_rec_var(filename);
+    nerrs += check_fix_rec_var(filename);
+    nerrs += check_rec_var(filename, 0);
+    nerrs += check_rec_var(filename, NC_64BIT_OFFSET);
+    nerrs += check_rec_var(filename, NC_64BIT_DATA);
     nerrs += check_not_last_var(filename);
     nerrs += check_add_var(filename);
     nerrs += check_var_offset(filename);
