b24df1f8a5dc229320ff6a1014644f7a5c4a81c9
Wei-keng Liao <wkliao@users.noreply.github.com>
2016-12-28 18:18:40 +0000

    update test/F90
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/branches/am@2761 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

---
+++ test/F90/seq_runs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+${TESTSEQRUN} ./test_intent  ${TESTOUTDIR}/testfile.nc
+${TESTSEQRUN} ./tst_f90      ${TESTOUTDIR}/testfile.nc
+${TESTSEQRUN} ./f90tst_vars  ${TESTOUTDIR}/testfile.nc
+${TESTSEQRUN} ./tst_types2   ${TESTOUTDIR}/testfile.nc
+${TESTSEQRUN} ./tst_f90_cdf5 ${TESTOUTDIR}/testfile.nc
+${TESTSEQRUN} ./f90tst_vars2 ${TESTOUTDIR}/testfile.nc
+${TESTSEQRUN} ./f90tst_vars3 ${TESTOUTDIR}/testfile.nc
+${TESTSEQRUN} ./f90tst_vars4 ${TESTOUTDIR}/testfile.nc
+${TESTSEQRUN} ./tst_flarge   ${TESTOUTDIR}/testfile.nc
+${TESTSEQRUN} ./tst_io       ${TESTOUTDIR}
+
