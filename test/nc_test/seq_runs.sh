6f8c58cee5e4824c2059b7a10e44c58a4cd7071c
Wei-keng Liao <wkliao@users.noreply.github.com>
2016-12-31 20:41:57 +0000

    another try to fix a strange error on travis ci that failed with message of option requires an argument -d
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/branches/am@2809 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- test/nc_test/seq_runs.sh
+++ test/nc_test/seq_runs.sh
@@ -5,12 +5,12 @@ set -e
 rm -f ${TESTOUTDIR}/scratch.nc
 rm -f ${TESTOUTDIR}/testfile.nc
 rm -f ${TESTOUTDIR}/tooth-fairy.nc
-${TESTSEQRUN} ./nc_test    -c -d${TESTOUTDIR}
-${TESTSEQRUN} ./nc_test       -d${TESTOUTDIR}
-${TESTSEQRUN} ./nc_test -2 -c -d${TESTOUTDIR}
-${TESTSEQRUN} ./nc_test -2    -d${TESTOUTDIR}
-${TESTSEQRUN} ./nc_test -5 -c -d${TESTOUTDIR}
-${TESTSEQRUN} ./nc_test -5    -d${TESTOUTDIR}
+${TESTSEQRUN} ./nc_test    -c -d ${TESTOUTDIR}
+${TESTSEQRUN} ./nc_test       -d ${TESTOUTDIR}
+${TESTSEQRUN} ./nc_test -2 -c -d ${TESTOUTDIR}
+${TESTSEQRUN} ./nc_test -2    -d ${TESTOUTDIR}
+${TESTSEQRUN} ./nc_test -5 -c -d ${TESTOUTDIR}
+${TESTSEQRUN} ./nc_test -5    -d ${TESTOUTDIR}
 ${TESTSEQRUN} ./t_nc             ${TESTOUTDIR}/testfile.nc
 ${TESTSEQRUN} ./tst_misc         ${TESTOUTDIR}/testfile.nc
 ${TESTSEQRUN} ./tst_norm         ${TESTOUTDIR}/testfile.nc
