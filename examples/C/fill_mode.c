31611190f0da673c6b98207c1f70cced439fe1d8
Wei-keng Liao <wkliao@users.noreply.github.com>
2017-03-04 19:53:07 +0000

    add more info in err printf
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/branches/am@2908 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- examples/C/fill_mode.c
+++ examples/C/fill_mode.c
@@ -171,9 +171,9 @@ int main(int argc, char** argv)
 
     err = ncmpi_inq_var_fill(ncid, fix_varid, &no_fill, &fill_value); ERR
     if (no_fill != 0)
-        printf("Error: expecting no_fill to be 0\n");
+        printf("Error at line %d: expecting no_fill to be 0\n", __LINE__);
     if (fill_value != NC_FILL_INT)
-        printf("Error: expecting no_fill to be %ld but got %d\n",NC_FILL_INT,fill_value);
+        printf("Error at line %d: expecting no_fill to be %ld but got %d\n", __LINE__,NC_FILL_INT,fill_value);
 
     /* fill the 1st record of the record variable */
     start[0] = 0;
