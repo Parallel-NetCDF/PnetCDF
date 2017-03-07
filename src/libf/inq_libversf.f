76f5a0305f2b87c456cbe904cf8da956d24a7b70
Wei-keng Liao <wkliao@users.noreply.github.com>
2017-03-07 00:16:51 +0000

    remove unused variables
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@2914 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- src/libf/inq_libversf.f
+++ src/libf/inq_libversf.f
@@ -1,10 +1,6 @@
       character *80 function nfmpi_inq_libvers()
-      integer ierr
       character *(80) tmpstr
 
-      integer nfmpi_xinq_libvers
-      external nfmpi_xinq_libvers
-      
-      ierr = nfmpi_xinq_libvers(tmpstr)
+      call nfmpi_xinq_libvers(tmpstr)
       nfmpi_inq_libvers = tmpstr
       end
