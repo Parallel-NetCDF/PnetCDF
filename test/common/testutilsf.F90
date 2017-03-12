207bcf8730922f4898a43c2e3f311ec3b3286d98
Wei-keng Liao <wkliao@users.noreply.github.com>
2017-03-12 22:24:31 +0000

    remove basename from executable file names
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/branches/am@2935 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- test/common/testutilsf.F90
+++ test/common/testutilsf.F90
@@ -16,11 +16,21 @@
           implicit none
           integer iargc
 #endif
-          integer argc
+          integer argc, i
           character(len=*) cmd, filename
+          character(len=256) full_cmd
 
           get_args = 1
-          call getarg(0, cmd)
+          call getarg(0, full_cmd)
+
+          ! remove basename from executable name
+          i = INDEX(full_cmd, "/", .TRUE.)
+          if (i .EQ. 0) then
+              cmd(:) = full_cmd(:)
+          else
+              cmd(:) = full_cmd(i+1:)
+          endif
+
           argc = IARGC()
           if (argc .GT. 1) then
               print*,'Usage: ',trim(cmd),' [filename]'
