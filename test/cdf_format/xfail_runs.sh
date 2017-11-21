ddedc919e507ff502db26d609bd880534853e617
Wei-keng Liao <wkliao@users.noreply.github.com>
2017-11-21 23:45:35 +0000

    add XFAIL_TESTS for using ncmpivalid to check bad files
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@3538 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

---
+++ test/cdf_format/xfail_runs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Copyright (C) 2003, Northwestern University and Argonne National Laboratory
+# See COPYRIGHT notice in top-level directory.
+#
+
+VALIDATOR=../../src/utils/ncmpivalid/ncmpivalid
+
+for i in ${BAD_FILES} ; do
+    ${TESTSEQRUN} ./${VALIDATOR} -q ${srcdir}/$i
+done
+
