c834a4b55c2ca1a0249f75f1d3a263f0c059aede
Wei-keng Liao <wkliao@users.noreply.github.com>
2017-11-22 00:42:28 +0000

    use wrapper wrap_runs.sh to run MPI programs and leave test programs need special treatment in seq_runs.sh
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@3542 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- test/nonblocking/seq_runs.sh
+++ test/nonblocking/seq_runs.sh
@@ -11,15 +11,6 @@ set -e
 
 VALIDATOR=../../src/utils/ncmpivalid/ncmpivalid
 
-for j in 0 1 ; do
-    export PNETCDF_SAFE_MODE=$j
-    for i in ${TESTPROGRAMS}; do
-        ${TESTSEQRUN} ./$i            ${TESTOUTDIR}/$i.nc
-        ${TESTSEQRUN} ${VALIDATOR} -q ${TESTOUTDIR}/$i.nc
-    done
-done
-
-
 ${TESTSEQRUN} ./mcoll_perf ${TESTOUTDIR}/testfile
 # seq is not available on FreeBSD otherwise we can use: for j in `seq 0 9`
 for j in 0 1 2 3 4 5 6 7 8 9 ; do
