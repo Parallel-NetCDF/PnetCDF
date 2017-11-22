900c9e735d48ded9ecc13bec4e1eed6cafe5d06b
Wei-keng Liao <wkliao@users.noreply.github.com>
2017-11-22 00:45:07 +0000

    use wrapper wrap_runs.sh to run MPI programs and leave test programs need special treatment in seq_runs.sh
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@3543 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- test/testcases/seq_runs.sh
+++ test/testcases/seq_runs.sh
@@ -9,14 +9,6 @@ set -e
 
 VALIDATOR=../../src/utils/ncmpivalid/ncmpivalid
 
-for j in 0 1 ; do
-    export PNETCDF_SAFE_MODE=$j
-    for i in ${TESTPROGRAMS}; do
-        ${TESTSEQRUN} ./$i            ${TESTOUTDIR}/$i.nc
-        ${TESTSEQRUN} ${VALIDATOR} -q ${TESTOUTDIR}/$i.nc
-    done
-done
-
 ${TESTSEQRUN} ./put_all_kinds ${TESTOUTDIR}/put_all_kinds.nc
 ${TESTSEQRUN} ${VALIDATOR} -q ${TESTOUTDIR}/put_all_kinds.nc.cdf1
 ${TESTSEQRUN} ${VALIDATOR} -q ${TESTOUTDIR}/put_all_kinds.nc.cdf2
