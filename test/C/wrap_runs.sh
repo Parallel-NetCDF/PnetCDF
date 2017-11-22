0804830d407d2927a460044f9173e5df02fb9640
Wei-keng Liao <wkliao@users.noreply.github.com>
2017-11-22 00:47:19 +0000

    use wrapper wrap_runs.sh to run MPI programs and add run dependency of pres_temp_4D_rd on pres_temp_4D_wr
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@3544 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- test/C/wrap_runs.sh
+++ test/C/wrap_runs.sh
@@ -11,8 +11,7 @@ VALIDATOR=../../src/utils/ncmpivalid/ncmpivalid
 
 for j in 0 1 ; do
     export PNETCDF_SAFE_MODE=$j
-    for i in ${TESTPROGRAMS} ; do
-        ${TESTSEQRUN} ./$i            ${TESTOUTDIR}/pres_temp_4D.nc
-        ${TESTSEQRUN} ${VALIDATOR} -q ${TESTOUTDIR}/pres_temp_4D.nc
-    done
+    echo "set PNETCDF_SAFE_MODE ${PNETCDF_SAFE_MODE}"
+    ${TESTSEQRUN} $1              ${TESTOUTDIR}/pres_temp_4D.nc
+    ${TESTSEQRUN} ${VALIDATOR} -q ${TESTOUTDIR}/pres_temp_4D.nc
 done
