695b3e35199798d6afc4012d8316f19916041c78
Wei-keng Liao <wkliao@users.noreply.github.com>
2017-12-15 19:40:16 +0000

    add end quote
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@3599 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- travis-run-tests.sh
+++ travis-run-tests.sh
@@ -5,5 +5,5 @@ make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS=--silent
 
 make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS="--silent --enable-shared LDFLAGS='-Wl,--allow-shlib-undefined'"
 
-make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS="--silent --enable-shared --disable-static LDFLAGS='-Wl,--allow-shlib-undefined'
+make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS="--silent --enable-shared --disable-static LDFLAGS='-Wl,--allow-shlib-undefined'"
 
