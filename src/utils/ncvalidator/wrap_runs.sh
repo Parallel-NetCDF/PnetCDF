61958a900b6def8e9b777bbb0055533e5d97120e
Wei-keng Liao <wkliao@users.noreply.github.com>
2017-12-26 04:31:36 +0000

    add test program tst_open.c to test ncmpi_open against corrupted files and checks expected error codes
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@3646 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

---
+++ src/utils/ncvalidator/wrap_runs.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#
+# Copyright (C) 2017, Northwestern University and Argonne National Laboratory
+# See COPYRIGHT notice in top-level directory.
+#
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+for i in ${ENULLPAD_FILES} ; do
+    for j in 0 1 ; do
+        export PNETCDF_SAFE_MODE=$j
+        # echo "set PNETCDF_SAFE_MODE ${PNETCDF_SAFE_MODE}"
+        ${TESTSEQRUN} ./tst_open ${srcdir}/$i NC_ENULLPAD
+    done
+done
+
+for i in ${EMAXVARS_FILES} ; do
+    for j in 0 1 ; do
+        export PNETCDF_SAFE_MODE=$j
+        # echo "set PNETCDF_SAFE_MODE ${PNETCDF_SAFE_MODE}"
+        ${TESTSEQRUN} ./tst_open ${srcdir}/$i NC_EMAXVARS
+    done
+done
+
+for i in ${EUNLIMIT_FILES} ; do
+    for j in 0 1 ; do
+        export PNETCDF_SAFE_MODE=$j
+        # echo "set PNETCDF_SAFE_MODE ${PNETCDF_SAFE_MODE}"
+        ${TESTSEQRUN} ./tst_open ${srcdir}/$i NC_EUNLIMIT
+    done
+done
+
+for i in ${ENOTNC_FILES} ; do
+    for j in 0 1 ; do
+        export PNETCDF_SAFE_MODE=$j
+        # echo "set PNETCDF_SAFE_MODE ${PNETCDF_SAFE_MODE}"
+        ${TESTSEQRUN} ./tst_open ${srcdir}/$i NC_ENOTNC
+    done
+done
+
