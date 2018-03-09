74fb8cba9acf71bc19b89c9156475fce6c2026a1
Wei-keng Liao <wkliao@users.noreply.github.com>
2018-03-09 06:34:58 +0000

    add configure command for travis ci to build pull request
    git-svn-id: https://svn.mcs.anl.gov/repos/parallel-netcdf/trunk@3704 c368ceb8-fd28-0410-a457-e46ee1d7cd4a

--- travis-run-tests.sh
+++ travis-run-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ev
 
+./configure
+
 make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS=--silent
 
 make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS="--silent --enable-shared LDFLAGS='-Wl,--allow-shlib-undefined'"
