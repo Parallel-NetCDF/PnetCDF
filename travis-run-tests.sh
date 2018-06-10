5912f302f0ef58338efa04904ad63e2dfc6e4cf2
Wei-keng Liao <wkliao@users.noreply.github.com>
2018-06-10 10:14:34 -0500

    add --enable-burst-buffering to Travis CI tests

--- travis-run-tests.sh
+++ travis-run-tests.sh
@@ -7,5 +7,5 @@ make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS="--silent"
 
 make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS="--silent --enable-shared"
 
-make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS="--silent --enable-shared --disable-static"
+make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS="--silent --enable-shared --enable-burst-buffering"
 
