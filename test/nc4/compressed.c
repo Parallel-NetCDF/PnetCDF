11093276863ab01da11f7e298af0a68c8b0d4368
Kaiyuan Hou <khou2020@outlook.com>
2018-07-27 13:19:00 -0500

    add support for zlib compression in netcdf4

---
+++ test/nc4/compressed.c
@@ -0,0 +1,74 @@
+/*
+   This program test reading a compressed netcdf 4 file
+   The data is written using NetCDF and read by PnetCDF
+   $Id$
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netcdf.h>
+#include <libgen.h> /* basename() */
+#include <pnetcdf.h>
+#include <mpi.h>
+#include <testutils.h>
+
+#define FNAME "gzip_example.nc"
+
+int main(int argc, char **argv) {
+    int i, err, nerrs = 0;
+    int rank, np;
+    int ncid;
+    int ndim, nvar;
+    int vid[32];
+    int did[2];
+    char *filename = FNAME;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (argc > 2) {
+        if (!rank) printf("Usage: %s [filename]\n",argv[0]);
+        MPI_Finalize();
+        return 1;
+    }
+    if (argc == 2) filename = argv[1];
+
+    if (rank == 0) {
+        char *cmd_str = (char*)malloc(strlen(argv[0]) + 256);
+        sprintf(cmd_str, "*** TESTING C   %s for interoperability file", basename(argv[0]));
+        printf("%-66s ------ ", cmd_str);
+        free(cmd_str);
+    }
+
+    /* Read with PnetCDF */
+    /* Open the file */
+    err = ncmpi_open(MPI_COMM_WORLD, filename, NC_NOWRITE, MPI_INFO_NULL, &ncid);
+    CHECK_ERR
+
+    err = ncmpi_inq(ncid, &ndim, &nvar, NULL, NULL);
+    CHECK_ERR
+
+    /* Close file */
+    ncmpi_close(ncid);
+
+    /* check if there is any malloc residue */
+    MPI_Offset malloc_size, sum_size;
+    err = ncmpi_inq_malloc_size(&malloc_size);
+    if (err == NC_NOERR) {
+        MPI_Reduce(&malloc_size, &sum_size, 1, MPI_OFFSET, MPI_SUM, 0, MPI_COMM_WORLD);
+        if (rank == 0 && sum_size > 0)
+            printf("heap memory allocated by PnetCDF internally has %lld bytes yet to be freed\n",
+                   sum_size);
+    }
+
+    MPI_Allreduce(MPI_IN_PLACE, &nerrs, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    if (rank == 0) {
+        if (nerrs) printf(FAIL_STR,nerrs);
+        else       printf(PASS_STR);
+    }
+
+   MPI_Finalize();
+   return (nerrs > 0);
+}
