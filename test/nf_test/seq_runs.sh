e4aa1021c467bf31f256a727ee06cb3e5b1a3697
Wei-keng Liao <wkliao@users.noreply.github.com>
2018-08-25 17:28:30 -0500

    less verbose output for "make check"

--- test/nf_test/seq_runs.sh
+++ test/nf_test/seq_runs.sh
@@ -41,7 +41,7 @@ ${TESTSEQRUN} ${VALIDATOR} -q ${TESTOUTDIR}/test.nc
 
 
 if test "x${ENABLE_BURST_BUFFER}" = x1 ; then
-    echo "---- testing burst buffering"
+    # echo "---- testing burst buffering"
 
     export PNETCDF_HINTS="nc_burst_buf=enable;nc_burst_buf_dirname=${TESTOUTDIR};nc_burst_buf_overwrite=enable"
     rm -f ${TESTOUTDIR}/test.nc
