6aca74a2f009c644b08b8d0cf704e3007ed5161d
Wei-keng Liao <wkliao@users.noreply.github.com>
2018-12-26 11:58:11 -0600

    add ptest to travis ci run script

--- travis-run-tests.sh
+++ travis-run-tests.sh
@@ -7,6 +7,8 @@ set -ev
 # Coverity Scan addons build_command already runs make distcheck
 # make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS="--silent"
 
+make -s V=1 LIBTOOLFLAGS=--silent ptest
+
 make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS="--silent --enable-shared"
 
 make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS="--silent --enable-shared --enable-burst-buffering"
