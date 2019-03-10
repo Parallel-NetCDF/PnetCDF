5833a87e0e98d2d3725578bb513668e7e5b42ccd
Kaiyuan Hou <khou2020@outlook.com>
2019-03-10 18:57:07 -0500

    fix footer check on BP file

--- src/utils/ncmpidump/ncmpidump.c
+++ src/utils/ncmpidump/ncmpidump.c
@@ -51,11 +51,7 @@ int main(int argc, char** argv);
 char *progname;
 
 #ifdef ENABLE_ADIOS 
-<<<<<<< HEAD
 unsigned int bp_ver;
-=======
-uint32_t bp_ver;
->>>>>>> 1882c45... version check
 #endif
 
 static void
