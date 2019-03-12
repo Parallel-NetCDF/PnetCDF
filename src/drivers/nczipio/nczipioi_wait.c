247e6761cc3e1ec60091a4c1bb29a40a3a6dc173
Kaiyuan Hou <khou2020@outlook.com>
2019-03-12 17:53:33 -0500

    nczipioi_wait

--- src/drivers/nczipio/nczipioi_wait.c
+++ src/drivers/nczipio/nczipioi_wait.c
@@ -30,13 +30,26 @@
 #include "nczipio_internal.h"
 
 
-int
-nczipioi_wait(NC_zip *nczipp, int nreqs, int *reqids, int *stats, int reqMode){
+/* We pack all request a s a large varn request
+ */
+int nczipioi_wait_put_reqs(NC_zip *nczipp, int nreqs, int *reqids, int *stats){
     int err;
+    int i;
+    int num = 0;
+    MPI_Offset **starts, **counts, **strides;
+    char **bufs;
     NC_zip *nczipp = (NC_zip*)ncdp;
+    NC_zip_req *req;
 
-    err = nczipp->driver->wait(nczipp->ncp, num_reqs, req_ids, statuses, reqMode);
-    if (err != NC_NOERR) return err;
+    // Count total number of vara request
+    for(i = 0; i < nreqs; i++){
+        req = nczipp->putlist.reqs[reqids[i]];
+        num += req->nreq;
+    }
+    
+    starts = (char**)NCI_Malloc(sizeof(char*) * num);
+    counts = (char**)NCI_Malloc(sizeof(char*) * num);
+    bufs =  (char**)NCI_Malloc(sizeof(char*) * num);
 
     return NC_NOERR;
 }
@@ -44,10 +57,77 @@ nczipioi_wait(NC_zip *nczipp, int nreqs, int *reqids, int *stats, int reqMode){
 int
 nczipioi_wait(NC_zip *nczipp, int nreqs, int *reqids, int *stats, int reqMode){
     int err;
+    int nput = 0, nread = 0;
+    int *putreqs = NULL, *getreqs = NULL;
+    int *putstats = NULL, *getstats = NULL;
     NC_zip *nczipp = (NC_zip*)ncdp;
 
-    err = nczipp->driver->wait(nczipp->ncp, num_reqs, req_ids, statuses, reqMode);
-    if (err != NC_NOERR) return err;
+    if (nreqs == NC_REQ_ALL || nreqs == NC_PUT_REQ_ALL){
+        nput = nczipp->putlist.nused;
+        putreqs = (int*)NCI_Malloc(sizeof(int) * nput);
+        memcpy(putreqs, nczipp->putlist.ids, nput * sizeof(int));
+    }
+    if(nreqs == NC_REQ_ALL || nreqs == NC_GET_REQ_ALL){
+        nget = nczipp->getlist.nused;
+        getreqs = (int*)NCI_Malloc(sizeof(int) * nget);
+        memcpy(getreqs, nczipp->getlist.ids, nget * sizeof(int));
+    }
+
+    if (nreqs > 0){
+        // Count number of get and put requests
+        for(i = 0; i < nreqs; i++){
+            if (reqids[i] & 1){
+                nput++;
+            }
+        }
+
+        // Allocate buffer
+        nget = nreqs - nput;
+        putreqs = (int*)NCI_Malloc(sizeof(int) * nput);
+        getreqs = (int*)NCI_Malloc(sizeof(int) * nget);
+        
+        // Build put and get req list
+        nput = nget = 0;
+        for(i = 0; i < nreqs; i++){
+            if (reqids[i] & 1){
+                putreqs[nput++] = reqids[i] >> 1;
+            }
+            else{
+                getreqs[nget++] = reqids[i] >> 1;
+            }
+        }
+    }
+
+    if (stats != NULL){
+        putstats = (int*)NCI_Malloc(sizeof(int) * nput);
+        getstats = (int*)NCI_Malloc(sizeof(int) * nget);
+    }
+    else{
+        putstats = NULL;
+        getstats = NULL;
+    }
+
+    nczipioi_wait_put_reqs(nczipp, nput, putreqs, putstats);
+    nczipioi_wait_put_reqs(nczipp, nput, putreqs, getstats);
+
+    // Assign stats
+    if (stats != NULL){
+        nput = nget = 0;
+        for(i = 0; i < nreqs; i++){
+            if (reqids[i] & 1){
+                stats[i] = putstats[nput++];
+            }
+            else{
+                stats[i] = getstats[nget++];
+            }
+        }
+
+        free(putstats);
+        free(getstats);
+    }
+
+    free(putreqs);
+    free(getreqs);
 
     return NC_NOERR;
 }
