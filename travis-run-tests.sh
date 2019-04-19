ebf6a6d7bd7d7f2bf57eed9352be5efa383c6f89
Wei-keng Liao <wkliao@users.noreply.github.com>
2019-04-19 00:35:57 -0500

    run configure and make before coverty scan starts

--- travis-run-tests.sh
+++ travis-run-tests.sh
@@ -7,9 +7,9 @@ set -ev
 # Coverity Scan addons build_command already runs make distcheck
 # make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS="--silent"
 
-make -s V=1 LIBTOOLFLAGS=--silent -j4 tests
+# make -s V=1 LIBTOOLFLAGS=--silent -j4 tests
 
-make -s V=1 LIBTOOLFLAGS=--silent ptest
+# make -s V=1 LIBTOOLFLAGS=--silent ptest
 
 make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS="--silent --enable-shared --enable-burst-buffering"
 
