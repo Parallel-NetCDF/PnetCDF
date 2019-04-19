d3d43cc67bfbd7fa8b696209e2ebdbc6476be63b
Wei-keng Liao <wkliao@users.noreply.github.com>
2019-04-19 01:26:17 -0500

    another adjust configure and make for travis-ci and coverty-scan

--- travis-run-tests.sh
+++ travis-run-tests.sh
@@ -7,8 +7,9 @@ set -ev
 # Coverity Scan addons build_command already runs make distcheck
 # make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS="--silent"
 
+make -s V=1 LIBTOOLFLAGS=--silent check
 make -s V=1 LIBTOOLFLAGS=--silent ptest
 
-make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS="--silent --enable-subfiling --enable-thread-safe --enable-burst_buffering --enable-shared --enable-debug"
+# make distcheck -s V=1 LIBTOOLFLAGS=--silent DISTCHECK_CONFIGURE_FLAGS="--silent --enable-subfiling --enable-thread-safe --enable-burst_buffering --enable-shared --enable-debug"
 
 make -s distclean
