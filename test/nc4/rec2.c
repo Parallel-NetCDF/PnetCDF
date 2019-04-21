b4c156daba78b09662bea5b41d393a0b2b8af7af
Wei-keng Liao <wkliao@users.noreply.github.com>
2019-04-20 19:46:49 -0500

    Call NetCDF-4 APIs first to create a file containing 2 unlimited dimensions. Then use PnetCDF APIs to open the file and do some checking

--- test/nc4/rec2.c
+++ test/nc4/rec2.c
@@ -1,8 +1,6 @@
 /*
  *  Copyright (C) 2019, Northwestern University and Argonne National Laboratory
  *  See COPYRIGHT notice in top-level directory.
- *
- *  $Id$
  */
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -17,6 +15,8 @@
 #include <string.h>
 #include <libgen.h> /* basename() */
 #include <mpi.h>
+#include <netcdf.h>
+#include <netcdf_par.h>
 #include <pnetcdf.h>
 
 #include <testutils.h>
@@ -24,38 +24,60 @@
 #define NY 8
 #define NX 5
 
-#define FNAME "two_rec_dims.nc"
-
 int main(int argc, char** argv) {
-    char *dir_name=".", filename[512];
-    int rank, nprocs, err, nerrs=0;
-    int ncid, num_rec_vars;
+    char filename[512];
+    int i, rank, nprocs, err, nerrs=0, buf[16];
+    int ncid, mode, dimids[2], varid, num_rec_vars;
     MPI_Comm comm=MPI_COMM_WORLD;
     MPI_Info info=MPI_INFO_NULL;
     MPI_Offset recsize;
+    size_t start[2], count[2];
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(comm, &rank);
     MPI_Comm_size(comm, &nprocs);
 
     if (argc > 2) {
-        if (!rank) printf("Usage: %s [dir_name]\n",argv[0]);
+        if (!rank) printf("Usage: %s [filename]\n",argv[0]);
         MPI_Finalize();
         return 1;
     }
-    if (argc == 2) dir_name = argv[1];
+    if (argc == 2) snprintf(filename, 256, "%s", argv[1]);
+    else           strcpy(filename, "testfile.nc");
+    MPI_Bcast(filename, 256, MPI_CHAR, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
         char *cmd_str = (char*)malloc(strlen(argv[0]) + 256);
-        sprintf(cmd_str, "*** TESTING C   %s for def_var_fill ", basename(argv[0]));
+        sprintf(cmd_str, "*** TESTING C   %s for reading file with 2 rec dims ", basename(argv[0]));
         printf("%-66s ------ ", cmd_str); fflush(stdout);
         free(cmd_str);
     }
 
-    sprintf(filename, "%s/%s", dir_name, FNAME);
+    /* create a NetCDF-4 file. Note NC_MPIIO is used in NetCDF 4.6.1 and
+     * earlier, but ignored in 4.6.2 and after. */
+    mode = NC_CLOBBER | NC_NETCDF4 | NC_MPIIO;
+    err = nc_create_par(filename, mode, comm, info, &ncid); CHECK_ERR
 
-    /* reopen the file and read data back */
-    err = ncmpi_open(comm, filename, NC_NOWRITE | NC_NETCDF4, info, &ncid); CHECK_ERR
+    err = nc_def_dim(ncid, "y", NC_UNLIMITED, &dimids[0]); CHECK_ERR
+    err = nc_def_dim(ncid, "x", NC_UNLIMITED, &dimids[1]); CHECK_ERR
+    err = nc_def_var(ncid, "data", NC_INT, 2, dimids, &varid); CHECK_ERR
+
+    err = nc_var_par_access(ncid, varid, NC_COLLECTIVE); CHECK_ERR
+
+    for (i=0; i<16; i++) buf[i] = rank + i;
+
+    /* No need to explicitly end define mode for netCDF-4 files */
+    start[0] = 0;
+    start[1] = 0;
+    count[0] = 2;
+    count[1] = 1;
+    err = nc_put_vara_int(ncid, varid, start, count, buf); CHECK_ERR
+
+    /* Close the file. */
+    err = nc_close(ncid); CHECK_ERR
+
+    /* reopen the file using PnetCDF */
+    err = ncmpi_open(comm, filename, NC_NOWRITE, info, &ncid); CHECK_ERR
 
     err = ncmpi_inq_num_rec_vars(ncid, &num_rec_vars); CHECK_ERR
     if (num_rec_vars != 1){
