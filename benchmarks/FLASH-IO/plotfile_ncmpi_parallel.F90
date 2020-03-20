1d84fa5d54ca9179da4a5b1a4ee3b92cc92287ed
wkliao <wkliao@users.noreply.github.com>
2020-03-19 23:38:20 -0500

    change array size for running more than 16K processes

--- benchmarks/FLASH-IO/plotfile_ncmpi_parallel.F90
+++ benchmarks/FLASH-IO/plotfile_ncmpi_parallel.F90
@@ -207,7 +207,7 @@
       integer ngid
       integer err
 
-      integer n_to_left(0:16383)  ! must extend from 0 to NumPEs-1
+      integer n_to_left(0:NumPEs)  ! must extend from 0 to NumPEs-1
 
 ! 2-20-00 -- we don't need to allocate more space than necessary
 !      integer gid(mfaces+1+mchild,maxblocks_tr)
@@ -215,7 +215,7 @@
 
       integer tot_blocks
 
-      save gid, n_to_left
+      save gid
 
       integer nzones_block(3)
 
