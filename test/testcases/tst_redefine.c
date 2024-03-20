b4fc0aceef6330dc20b6b9d0db899b9ebec0ab5f
wkliao <wkliao@users.noreply.github.com>
2024-03-20 15:04:02 -0500

    read back variables and check contents

--- test/testcases/tst_redefine.c
+++ test/testcases/tst_redefine.c
@@ -27,11 +27,84 @@
 
 static int verbose;
 
+#define LEN 16
+
+#define CHECK_VAL(ncid, varid, ii, val, expect) {                     \
+    if (val != expect) {                                              \
+        char name[16];                                                \
+        err = ncmpi_inq_varname(ncid, varid, name);                   \
+        CHECK_ERROUT                                                  \
+        printf("%s line %d: var %s i=%d expecting %d but got %d\n",   \
+               __func__,__LINE__,name,ii,expect,val);                 \
+        nerrs++;                                                      \
+        goto err_out;                                                 \
+    }                                                                 \
+}
+
+static int
+check_fix_vars(MPI_Comm comm, int ncid, int *varid)
+{
+    int i, nerrs=0, err, rank, nprocs, *buf;
+    MPI_Offset start[2], count[2];
+
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &nprocs);
+
+    start[0] = 0; start[1] = rank * (LEN / nprocs);
+    count[0] = 1; count[1] = LEN / nprocs;
+
+    buf = (int*) malloc(sizeof(int) * count[0] * count[1]);
+
+    for (i=0; i<count[0]*count[1]; i++) buf[i] = -1;
+    err = ncmpi_get_vara_int_all(ncid, varid[0], start+1, count+1, buf); CHECK_ERROUT
+    for (i=0; i<LEN/nprocs; i++)
+        CHECK_VAL(ncid, varid[0], i, buf[i], rank+i)
+
+    for (i=0; i<count[0]*count[1]; i++) buf[i] = -1;
+    err = ncmpi_get_vara_int_all(ncid, varid[1], start+1, count+1, buf); CHECK_ERROUT
+    for (i=0; i<LEN/nprocs; i++)
+        CHECK_VAL(ncid, varid[1], i, buf[i], rank+i+100)
+
+err_out:
+    free(buf);
+    return nerrs;
+}
+
+static int
+check_rec_vars(MPI_Comm comm, int ncid, int *varid)
+{
+    int i, nerrs=0, err, rank, nprocs, *buf;
+    MPI_Offset start[2], count[2];
+
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &nprocs);
+
+    start[0] = 0; start[1] = rank * (LEN / nprocs);
+    count[0] = 2; count[1] = LEN / nprocs;
+
+    buf = (int*) malloc(sizeof(int) * count[0] * count[1]);
+
+    for (i=0; i<count[0]*count[1]; i++) buf[i] = -1;
+    err = ncmpi_get_vara_int_all(ncid, varid[0], start,   count,   buf); CHECK_ERROUT
+    for (i=0; i<2*LEN/nprocs; i++)
+        CHECK_VAL(ncid, varid[0], i, buf[i], rank+i+1000)
+
+    for (i=0; i<count[0]*count[1]; i++) buf[i] = -1;
+    err = ncmpi_get_vara_int_all(ncid, varid[1], start,   count,   buf); CHECK_ERROUT
+    for (i=0; i<2*LEN/nprocs; i++)
+        CHECK_VAL(ncid, varid[1], i, buf[i], rank+i+10000)
+
+err_out:
+    free(buf);
+    return nerrs;
+}
+
 static int
 tst_fmt(char *filename, int cmode)
 {
-    int rank, ncid, err, nerrs=0;
-    int dimid[3], varid[4], minfree=100;
+    int i, rank, nprocs, ncid, err, nerrs=0;
+    int *buf, dimid[3], varid[4], minfree=100;
+    MPI_Offset start[2], count[2];
     MPI_Offset old_hsize, hsize;
     MPI_Offset old_extent, extent;
     MPI_Offset old_var_off, var_off;
@@ -39,6 +112,7 @@ tst_fmt(char *filename, int cmode)
     MPI_Comm comm = MPI_COMM_WORLD;
 
     MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &nprocs);
 
     unsetenv("PNETCDF_HINTS");
 
@@ -47,7 +121,7 @@ tst_fmt(char *filename, int cmode)
     err = ncmpi_create(comm, filename, cmode, MPI_INFO_NULL, &ncid); CHECK_ERR
 
     err = ncmpi_def_dim(ncid, "time", NC_UNLIMITED, &dimid[0]); CHECK_ERR
-    err = ncmpi_def_dim(ncid, "dim", 25, &dimid[1]); CHECK_ERR
+    err = ncmpi_def_dim(ncid, "dim", LEN, &dimid[1]); CHECK_ERR
     err = ncmpi_def_var(ncid, "fa", NC_INT, 1, dimid+1, &varid[0]); CHECK_ERR
     err = ncmpi_def_var(ncid, "fb", NC_INT, 1, dimid+1, &varid[1]); CHECK_ERR
     err = ncmpi_def_var(ncid, "ta", NC_INT, 2, dimid,   &varid[2]); CHECK_ERR
@@ -56,10 +130,34 @@ tst_fmt(char *filename, int cmode)
     /* disable header alignment */
     err = ncmpi__enddef(ncid, 0, 4, 0, 4); CHECK_ERR
 
+    start[0] = 0; start[1] = rank * (LEN / nprocs);
+    count[0] = 2; count[1] = LEN / nprocs;
+
+    buf = (int*) malloc(sizeof(int) * count[0] * count[1]);
+
+    for (i=0; i<LEN/nprocs; i++) buf[i] = rank + i;
+    err = ncmpi_put_vara_int_all(ncid, varid[0], start+1, count+1, buf); CHECK_ERR
+    for (i=0; i<LEN/nprocs; i++) buf[i] = rank + i + 100;
+    err = ncmpi_put_vara_int_all(ncid, varid[1], start+1, count+1, buf); CHECK_ERR
+    for (i=0; i<2*LEN/nprocs; i++) buf[i] = rank + i + 1000;
+    err = ncmpi_put_vara_int_all(ncid, varid[2], start,   count,   buf); CHECK_ERR
+    for (i=0; i<2*LEN/nprocs; i++) buf[i] = rank + i + 10000;
+    err = ncmpi_put_vara_int_all(ncid, varid[3], start,   count,   buf); CHECK_ERR
+
+    err = check_fix_vars(comm, ncid, varid);
+    if (err > 0) goto err_out;
+    err = check_rec_vars(comm, ncid, varid+2);
+    if (err > 0) goto err_out;
+
     err = ncmpi_close(ncid); CHECK_ERR
 
     /* reopen the file and check file header size and extent */
     err = ncmpi_open(comm, filename, NC_WRITE, MPI_INFO_NULL, &ncid); CHECK_ERR
+
+    err = ncmpi_inq_varid(ncid, "fa", &varid[0]); CHECK_ERR
+    err = ncmpi_inq_varid(ncid, "fb", &varid[1]); CHECK_ERR
+    err = ncmpi_inq_varid(ncid, "ta", &varid[2]); CHECK_ERR
+    err = ncmpi_inq_varid(ncid, "tb", &varid[3]); CHECK_ERR
 
     err = ncmpi_inq_header_size(ncid, &hsize); CHECK_ERR
     err = ncmpi_inq_header_extent(ncid, &extent); CHECK_ERR
@@ -103,6 +201,11 @@ tst_fmt(char *filename, int cmode)
                __LINE__,__FILE__, extent, old_extent);
     }
 
+    err = check_fix_vars(comm, ncid, varid);
+    if (err > 0) goto err_out;
+    err = check_rec_vars(comm, ncid, varid+2);
+    if (err > 0) goto err_out;
+
     /* enter redefine mode and add nothing */
     err = ncmpi_redef(ncid); CHECK_ERR
 
@@ -130,6 +233,11 @@ tst_fmt(char *filename, int cmode)
         printf("Error at line %d in %s: File header extent %lld fails to grow into %lld\n",
                __LINE__,__FILE__, extent, old_extent + minfree);
     }
+
+    err = check_fix_vars(comm, ncid, varid);
+    if (err > 0) goto err_out;
+    err = check_rec_vars(comm, ncid, varid+2);
+    if (err > 0) goto err_out;
 
     /* enter redefine mode and add nothing */
     err = ncmpi_redef(ncid); CHECK_ERR
@@ -159,6 +267,11 @@ tst_fmt(char *filename, int cmode)
                __LINE__,__FILE__, extent);
     }
 
+    err = check_fix_vars(comm, ncid, varid);
+    if (err > 0) goto err_out;
+    err = check_rec_vars(comm, ncid, varid+2);
+    if (err > 0) goto err_out;
+
     err = ncmpi_close(ncid); CHECK_ERR
 
 
@@ -166,6 +279,11 @@ tst_fmt(char *filename, int cmode)
 
     /* reopen the file and check file header size and extent */
     err = ncmpi_open(comm, filename, NC_WRITE, MPI_INFO_NULL, &ncid); CHECK_ERR
+
+    err = ncmpi_inq_varid(ncid, "fa", &varid[0]); CHECK_ERR
+    err = ncmpi_inq_varid(ncid, "fb", &varid[1]); CHECK_ERR
+    err = ncmpi_inq_varid(ncid, "ta", &varid[2]); CHECK_ERR
+    err = ncmpi_inq_varid(ncid, "tb", &varid[3]); CHECK_ERR
 
     /* enter redefine mode and add nothing */
     err = ncmpi_redef(ncid); CHECK_ERR
@@ -198,6 +316,11 @@ tst_fmt(char *filename, int cmode)
 
     unsetenv("PNETCDF_HINTS");
 
+    err = check_fix_vars(comm, ncid, varid);
+    if (err > 0) goto err_out;
+    err = check_rec_vars(comm, ncid, varid+2);
+    if (err > 0) goto err_out;
+
     /* enter redefine mode and add nothing */
     err = ncmpi_redef(ncid); CHECK_ERR
 
@@ -229,6 +352,11 @@ tst_fmt(char *filename, int cmode)
     /* obtain 1st record variable's file offset */
     err = ncmpi_inq_varoffset(ncid, varid[2], &old_var_off); CHECK_ERR
 
+    err = check_fix_vars(comm, ncid, varid);
+    if (err > 0) goto err_out;
+    err = check_rec_vars(comm, ncid, varid+2);
+    if (err > 0) goto err_out;
+
     /* enter redefine mode and add nothing */
     err = ncmpi_redef(ncid); CHECK_ERR
 
@@ -244,6 +372,11 @@ tst_fmt(char *filename, int cmode)
         printf("Error at line %d in %s: 1st record variable offset %lld (expecting %lld)\n",
                __LINE__,__FILE__, var_off, old_var_off+400);
     }
+
+    err = check_fix_vars(comm, ncid, varid);
+    if (err > 0) goto err_out;
+    err = check_rec_vars(comm, ncid, varid+2);
+    if (err > 0) goto err_out;
 
 #if 0
     err = ncmpi_close(ncid); CHECK_ERR
@@ -268,6 +401,11 @@ tst_fmt(char *filename, int cmode)
                __LINE__,__FILE__, var_off, r_align);
     }
 
+    err = check_fix_vars(comm, ncid, varid);
+    if (err > 0) goto err_out;
+    err = check_rec_vars(comm, ncid, varid+2);
+    if (err > 0) goto err_out;
+
     err = ncmpi_close(ncid); CHECK_ERR
 
     unsetenv("PNETCDF_HINTS");
@@ -278,7 +416,7 @@ tst_fmt(char *filename, int cmode)
 
     /* define only record variables */
     err = ncmpi_def_dim(ncid, "time", NC_UNLIMITED, &dimid[0]); CHECK_ERR
-    err = ncmpi_def_dim(ncid, "dim", 25, &dimid[1]); CHECK_ERR
+    err = ncmpi_def_dim(ncid, "dim", LEN, &dimid[1]); CHECK_ERR
     err = ncmpi_def_var(ncid, "ta", NC_INT, 2, dimid,   &varid[2]); CHECK_ERR
     err = ncmpi_def_var(ncid, "tb", NC_INT, 2, dimid,   &varid[3]); CHECK_ERR
 
@@ -288,6 +426,14 @@ tst_fmt(char *filename, int cmode)
     v_align = 100;
     r_align = 512;
     err = ncmpi__enddef(ncid, 0, v_align, 0, r_align); CHECK_ERR
+
+    start[0] = 0; start[1] = rank * (LEN / nprocs);
+    count[0] = 2; count[1] = LEN / nprocs;
+
+    for (i=0; i<2*LEN/nprocs; i++) buf[i] = rank + i + 1000;
+    err = ncmpi_put_vara_int_all(ncid, varid[2], start,   count,   buf); CHECK_ERR
+    for (i=0; i<2*LEN/nprocs; i++) buf[i] = rank + i + 10000;
+    err = ncmpi_put_vara_int_all(ncid, varid[3], start,   count,   buf); CHECK_ERR
 
     err = ncmpi_inq_header_size(ncid, &hsize); CHECK_ERR
     err = ncmpi_inq_header_extent(ncid, &extent); CHECK_ERR
@@ -309,8 +455,13 @@ tst_fmt(char *filename, int cmode)
                __LINE__,__FILE__, extent, r_align);
     }
 
-    err = ncmpi_close(ncid); CHECK_ERR
+    err = check_rec_vars(comm, ncid, varid+2);
+    if (err > 0) goto err_out;
 
+    err = ncmpi_close(ncid); CHECK_ERR
+    free(buf);
+
+err_out:
     return nerrs;
 }
 
