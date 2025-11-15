b72b67f439d89b4fe616d810263a36f026b8db4c
wkliao <wkliao@users.noreply.github.com>
2025-11-14 22:40:08 -0600

    flexible_bput.c add testing for indep I/O mode

--- test/nonblocking/flexible_bput.c
+++ test/nonblocking/flexible_bput.c
@@ -21,7 +21,7 @@
  *
  *    % mpicc -O2 -o flexible_bput flexible_bput.c -lpnetcdf
  *
- *    % mpiexec -l -n 4 ./flexible_bput /pvfs2/wkliao/testfile.nc
+ *    % mpiexec -l -n 4 ./flexible_bput -l 4 /pvfs2/wkliao/testfile.nc
  *
  *    % ncmpidump /pvfs2/wkliao/testfile.nc
  *    netcdf testfile {
@@ -48,14 +48,16 @@
 #include <stdlib.h>
 #include <string.h> /* strcpy() */
 #include <libgen.h> /* basename() */
+#include <unistd.h> /* getopt() */
 #include <assert.h>
+
 #include <mpi.h>
 #include <pnetcdf.h>
 
 #include <testutils.h>
 
 #define NY 6
-#define NX 4
+#define NX 70
 #define GHOST 2
 
 #define INIT_PUT_BUF(buf) \
@@ -78,6 +80,7 @@
                     printf("Error at line %d in %s: put buffer altered buffer[%d][%d]=%f\n", \
                            __LINE__,__FILE__,i,j,(double)buf[i][j]); \
                     nerrs++; \
+                    goto err_out; \
                 } \
             } \
             else { \
@@ -85,6 +88,7 @@
                     printf("Error at line %d in %s: put buffer altered buffer[%d][%d]=%f\n", \
                            __LINE__,__FILE__,i,j,(double)buf[i][j]); \
                     nerrs++; \
+                    goto err_out; \
                 } \
             } \
         } \
@@ -101,51 +105,47 @@
             if (i < GHOST || GHOST+array_of_subsizes[0] <= i || \
                 j < GHOST || GHOST+array_of_subsizes[1] <= j) { \
                 if (buf[i][j] != -2) { \
-                    printf("Error at line %d in %s: unexpected get buffer[%d][%d]=%f\n", \
-                           __LINE__,__FILE__,i,j,(double)buf[i][j]); \
+                    printf("Error at line %d in %s: expect buffer[%d][%d] to be %d but got %f\n", \
+                           __LINE__,__FILE__,i,j,-2,(double)buf[i][j]); \
                     nerrs++; \
+                    goto err_out; \
                 } \
             } \
             else { \
-                if (buf[i][j] != (i-GHOST)*array_of_subsizes[1]+(j-GHOST)) { \
-                    printf("Error at line %d in %s: unexpected get buffer[%d][%d]=%f\n", \
-                           __LINE__,__FILE__,i,j,(double)buf[i][j]); \
+                int exp = (i-GHOST)*array_of_subsizes[1]+(j-GHOST); \
+                if (buf[i][j] != exp) { \
+                    printf("Error at line %d in %s: expect buffer[%d][%d] to be %d but got %f\n", \
+                           __LINE__,__FILE__,i,j,exp,(double)buf[i][j]); \
                     nerrs++; \
+                    goto err_out; \
                 } \
             } \
         } \
     }
 
-int main(int argc, char** argv)
+static
+int tst_run(const char *filename, int coll_io, int len)
 {
-    char filename[256];
     int i, j, rank, nprocs, err, nerrs=0, req, status;
     int ncid, cmode, varid, dimid[2];
     int array_of_sizes[2], array_of_subsizes[2], array_of_starts[2];
-    int    buf_int[NX+2*GHOST][NY+2*GHOST];
-    double buf_dbl[NX+2*GHOST][NY+2*GHOST];
+    int    **buf_int;
+    double **buf_dbl;
     MPI_Offset start[2], count[2], stride[2], imap[2];
     MPI_Datatype  subarray;
 
-    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-    if (argc > 2) {
-        if (!rank) printf("Usage: %s [filename]\n",argv[0]);
-        MPI_Finalize();
-        return 1;
-    }
-    if (argc == 2) snprintf(filename, 256, "%s", argv[1]);
-    else           strcpy(filename, "testfile.nc");
-    MPI_Bcast(filename, 256, MPI_CHAR, 0, MPI_COMM_WORLD);
+    buf_int = (int**) malloc(sizeof(int*) * (len+2*GHOST));
+    buf_int[0] = (int*) malloc(sizeof(int) * (len+2*GHOST) * (NY+2*GHOST));
+    for (i=1; i<(len+2*GHOST); i++)
+        buf_int[i] = buf_int[i-1] + (NY+2*GHOST);
 
-    if (rank == 0) {
-        char *cmd_str = (char*)malloc(strlen(argv[0]) + 256);
-        sprintf(cmd_str, "*** TESTING C   %s for flexible bput_varm ", basename(argv[0]));
-        printf("%-66s ------ ", cmd_str);
-        free(cmd_str);
-    }
+    buf_dbl = (double**) malloc(sizeof(double*) * (len+2*GHOST));
+    buf_dbl[0] = (double*) malloc(sizeof(double) * (len+2*GHOST) * (NY+2*GHOST));
+    for (i=1; i<(len+2*GHOST); i++)
+        buf_dbl[i] = buf_dbl[i-1] + (NY+2*GHOST);
 
     /* create a new file for writing ----------------------------------------*/
     cmode = NC_CLOBBER | NC_64BIT_DATA;
@@ -153,22 +153,27 @@ int main(int argc, char** argv)
     CHECK_ERR
 
     /* define 2 dimensions */
-    err = ncmpi_def_dim(ncid, "Y", NY,        &dimid[0]); CHECK_ERR
-    err = ncmpi_def_dim(ncid, "X", NX*nprocs, &dimid[1]); CHECK_ERR
+    err = ncmpi_def_dim(ncid, "Y", NY,         &dimid[0]); CHECK_ERR
+    err = ncmpi_def_dim(ncid, "X", len*nprocs, &dimid[1]); CHECK_ERR
 
-    /* define a variable of size NY * (NX * nprocs) */
+    /* define a variable of size NY * (len * nprocs) */
     err = ncmpi_def_var(ncid, "var", NC_DOUBLE, 2, dimid, &varid); CHECK_ERR
     err = ncmpi_enddef(ncid); CHECK_ERR
 
-     start[0] = 0;  start[1] = NX * rank;
-     count[0] = NY; count[1] = NX;
+    if (!coll_io) {
+        err = ncmpi_begin_indep_data(ncid);
+        CHECK_ERR
+    }
+
+     start[0] = 0;  start[1] = len * rank;
+     count[0] = NY; count[1] = len;
     stride[0] = 1; stride[1] = 1;
-      imap[0] = 1;   imap[1] = NY; /* would be {NX, 1} if not transposing */
+      imap[0] = 1;   imap[1] = NY; /* would be {len, 1} if not transposing */
 
     /* var is partitioned along X dimension in a matrix transported way */
-    array_of_sizes[0]    = NX + 2*GHOST;
+    array_of_sizes[0]    = len + 2*GHOST;
     array_of_sizes[1]    = NY + 2*GHOST;
-    array_of_subsizes[0] = NX;
+    array_of_subsizes[0] = len;
     array_of_subsizes[1] = NY;
     array_of_starts[0]   = GHOST;
     array_of_starts[1]   = GHOST;
@@ -184,13 +189,18 @@ int main(int argc, char** argv)
     for (i=0; i<2; i++) bufsize *= count[i];
     err = ncmpi_buffer_attach(ncid, bufsize); CHECK_ERR
 
-    err = ncmpi_bput_varm(ncid, varid, start, count, stride, imap, buf_int,
+    err = ncmpi_bput_varm(ncid, varid, start, count, stride, imap, buf_int[0],
                           1, subarray, &req);
     CHECK_ERR
     /* check if the contents of put buffer are altered */
     CHECK_PUT_BUF(buf_int)
 
-    err = ncmpi_wait_all(ncid, 1, &req, &status); CHECK_ERR
+    if (coll_io)
+        err = ncmpi_wait_all(ncid, 1, &req, &status);
+    else
+        err = ncmpi_wait(ncid, 1, &req, &status);
+
+    CHECK_ERR
     err = status; CHECK_ERR
 
     /* check the contents of put buffer are altered */
@@ -202,10 +212,19 @@ int main(int argc, char** argv)
     /* initiate get buffer contents */
     INIT_GET_BUF(buf_int)
 
-    /* calling a blocking flexible API */
-    err = ncmpi_get_varm_all(ncid, varid, start, count, stride, imap, buf_int,
-                             1, subarray);
+    if (!coll_io) {
+        /* calling a blocking flexible API */
+        err = ncmpi_end_indep_data(ncid);
+        CHECK_ERR
+    }
+    err = ncmpi_get_varm_all(ncid, varid, start, count, stride, imap,
+                             buf_int[0], 1, subarray);
     CHECK_ERR
+
+    if (!coll_io) {
+        err = ncmpi_begin_indep_data(ncid);
+        CHECK_ERR
+    }
 
     /* check the contents of get buffer */
     CHECK_GET_BUF(buf_int)
@@ -224,14 +243,18 @@ int main(int argc, char** argv)
 
     err = ncmpi_buffer_attach(ncid, bufsize); CHECK_ERR
 
-    err = ncmpi_bput_varm(ncid, varid, start, count, stride, imap, buf_dbl,
+    err = ncmpi_bput_varm(ncid, varid, start, count, stride, imap, buf_dbl[0],
                           1, subarray, &req);
     CHECK_ERR
 
     /* check the contents of put buffer are altered */
     CHECK_PUT_BUF(buf_dbl)
 
-    err = ncmpi_wait_all(ncid, 1, &req, &status); CHECK_ERR
+    if (coll_io)
+        err = ncmpi_wait_all(ncid, 1, &req, &status);
+    else
+        err = ncmpi_wait(ncid, 1, &req, &status);
+    CHECK_ERR
     err = status; CHECK_ERR
 
     /* check the contents of put buffer are altered */
@@ -244,16 +267,79 @@ int main(int argc, char** argv)
     INIT_GET_BUF(buf_dbl)
 
     /* calling a blocking flexible API */
-    err = ncmpi_get_varm_all(ncid, varid, start, count, stride, imap, buf_dbl,
-                             1, subarray);
+    if (coll_io)
+        err = ncmpi_get_varm_all(ncid, varid, start, count, stride, imap,
+                                 buf_dbl[0], 1, subarray);
+    else
+        err = ncmpi_get_varm(ncid, varid, start, count, stride, imap,
+                                 buf_dbl[0], 1, subarray);
     CHECK_ERR
 
     /* check the contents of get buffer */
     CHECK_GET_BUF(buf_dbl)
 
+err_out:
     MPI_Type_free(&subarray);
 
     err = ncmpi_close(ncid); CHECK_ERR
+
+    free(buf_int[0]);
+    free(buf_int);
+    free(buf_dbl[0]);
+    free(buf_dbl);
+
+    return nerrs;
+}
+
+#define FILE_NAME "testfile.nc"
+
+static void
+usage(char *argv0)
+{
+    char *help =
+    "Usage: %s [OPTIONS]...[filename]\n"
+    "       [-h] Print help\n"
+    "       [-l num]: X dimension size of local array\n"
+    "       [filename]: output netCDF file name (default: %s)\n";
+    fprintf(stderr, help, argv0, FILE_NAME);
+}
+
+int main(int argc, char** argv)
+{
+    extern int optind;
+    extern char *optarg;
+    char filename[256];
+    int i, len, rank, nprocs, nerrs=0, err=0;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    len = NX;
+    while ((i = getopt(argc, argv, "hl:")) != EOF)
+        switch(i) {
+            case 'l': len = atoi(optarg);
+                      break;
+            case 'h':
+            default:  if (rank==0) usage(argv[0]);
+                      MPI_Finalize();
+                      return 1;
+        }
+    if (argv[optind] == NULL) strcpy(filename, FILE_NAME);
+    else                      snprintf(filename, 256, "%s", argv[optind]);
+
+    if (rank == 0) {
+        char *cmd_str = (char*)malloc(strlen(argv[0]) + 256);
+        sprintf(cmd_str, "*** TESTING C   %s for flexible bput_varm ", basename(argv[0]));
+        printf("%-66s ------ ", cmd_str);
+        free(cmd_str);
+    }
+
+    nerrs = tst_run(filename, 0, len);
+    if (nerrs != 0) goto err_out;
+
+    nerrs = tst_run(filename, 1, len);
+    if (nerrs != 0) goto err_out;
 
     /* check if PnetCDF freed all internal malloc */
     MPI_Offset malloc_size, sum_size;
@@ -267,6 +353,7 @@ int main(int argc, char** argv)
         if (malloc_size > 0) ncmpi_inq_malloc_list();
     }
 
+err_out:
     MPI_Allreduce(MPI_IN_PLACE, &nerrs, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
     if (rank == 0) {
         if (nerrs) printf(FAIL_STR,nerrs);
