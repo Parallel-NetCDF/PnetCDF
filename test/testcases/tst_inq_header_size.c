05249da0504a487b1dd6b58106e997b0dbe728b1
wkliao <wkliao@users.noreply.github.com>
2025-12-25 00:20:47 -0600

    align print line

--- test/testcases/tst_inq_header_size.c
+++ test/testcases/tst_inq_header_size.c
@@ -239,7 +239,7 @@ int main(int argc, char **argv) {
     if (rank == 0) {
         char *cmd_str = (char*)malloc(strlen(argv[0]) + 256);
         sprintf(cmd_str, "*** TESTING C   %s for get header size in define mode", basename(argv[0]));
-        printf("%-66s ------ ", cmd_str); fflush(stdout);
+        printf("%-66s -- ", cmd_str); fflush(stdout);
         free(cmd_str);
     }
 
