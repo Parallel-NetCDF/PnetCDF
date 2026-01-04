43148348f4dd5a88bdf61769333169e2ad8965c2
wkliao <wkliao@users.noreply.github.com>
2026-01-04 15:05:09 -0600

    Separate ncmpidiff core subroutines into a file and build it as a library
    This enables PnetCDF test programs to call the ncmpidiff core subroutine
    to make comparison of two files.

---
+++ src/utils/ncmpidiff/ncmpidiff_core.h
@@ -0,0 +1,23 @@
+/*
+ *  Copyright (C) 2025, Northwestern University and Argonne National Laboratory
+ *  See COPYRIGHT notice in top-level directory.
+ */
+
+#include <stdlib.h>
+
+int ncmpidiff_core(const char  *file1,
+                   const char  *file2,
+                   MPI_Comm     comm,
+                   MPI_Info     info,
+                   int          verbose,
+                   int          quiet,
+                   int          check_header,
+                   int          check_variable_list,
+                   int          check_entire_file,
+                   int          num_vars,
+                   char       **var_names,
+                   int          check_tolerance,
+                   int          first_diff,
+                   char        *cmd_opts,
+                   double       tolerance_difference,
+                   double       tolerance_ratio);
