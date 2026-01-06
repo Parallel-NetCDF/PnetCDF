0eb6737e74fda6c26cc591745686c63412705cd2
wkliao <wkliao@users.noreply.github.com>
2026-01-06 15:03:47 -0600

    ncmpidiff_core() fix data type MPI_Offset of return value (number of differences)

--- src/utils/ncmpidiff/ncmpidiff_core.c
+++ src/utils/ncmpidiff/ncmpidiff_core.c
@@ -308,22 +308,22 @@ get_type(int type)
 }
 
 /*----< ncmpidiff_core() >---------------------------------------------------*/
-int ncmpidiff_core(const char  *file1,
-                   const char  *file2,
-                   MPI_Comm     comm,
-                   MPI_Info     info,
-                   int          verbose,
-                   int          quiet,
-                   int          check_header,
-                   int          check_variable_list,
-                   int          check_entire_file,
-                   int          num_vars,
-                   char       **var_names,
-                   int          check_tolerance,
-                   int          first_diff,
-                   char        *cmd_opts,
-                   double       tolerance_difference,
-                   double       tolerance_ratio)
+MPI_Offset ncmpidiff_core(const char  *file1,
+                          const char  *file2,
+                          MPI_Comm     comm,
+                          MPI_Info     info,
+                          int          verbose,
+                          int          quiet,
+                          int          check_header,
+                          int          check_variable_list,
+                          int          check_entire_file,
+                          int          num_vars,
+                          char       **var_names,
+                          int          check_tolerance,
+                          int          first_diff,
+                          char        *cmd_opts,
+                          double       tolerance_difference,
+                          double       tolerance_ratio)
 {
     char *name[2];
     int i, j, err, rank, nprocs;
