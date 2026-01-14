4ecee6fbc4e5758402a811faf19c3b9b216be5b2
wkliao <wkliao@users.noreply.github.com>
2026-01-14 12:47:29 -0600

    test/cdf_format make use of tst_main()

--- test/cdf_format/parallel_run.sh
+++ test/cdf_format/parallel_run.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 #
-# Copyright (C) 2018, Northwestern University and Argonne National Laboratory
+# Copyright (C) 2026, Northwestern University and Argonne National Laboratory
 # See COPYRIGHT notice in top-level directory.
 #
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-VALIDATOR=../../src/utils/ncvalidator/ncvalidator
-NCMPIDIFF=../../src/utils/ncmpidiff/ncmpidiff
+DRY_RUN=no
+VERBOSE=no
 
-# remove file system type prefix if there is any
-OUTDIR=`echo "$TESTOUTDIR" | cut -d: -f2-`
+run_cmd() {
+   local lineno=${BASH_LINENO[$((${#BASH_LINENO[@]} - 2))]}
+   if test "x$VERBOSE" = xyes || test "x$DRY_RUN" = xyes ; then
+      echo "Line $lineno CMD: $MPIRUN $@"
+   fi
+   if test "x$DRY_RUN" = xno ; then
+      $MPIRUN $@
+   fi
+}
 
 MPIRUN=`echo ${TESTMPIRUN} | ${SED} -e "s/NP/$1/g"`
 # echo "MPIRUN = ${MPIRUN}"
 # echo "check_PROGRAMS=${check_PROGRAMS}"
-# echo "srcdir = ${srcdir}"
 
-# echo "PNETCDF_DEBUG = ${PNETCDF_DEBUG}"
 if test "x${PNETCDF_DEBUG}" = x1 ; then
    safe_modes="0 1"
 else
@@ -28,81 +33,35 @@ fi
 # prevent user environment setting of PNETCDF_HINTS to interfere
 unset PNETCDF_HINTS
 
-for j in ${safe_modes} ; do
-        if test "$j" = 1 ; then # test only in safe mode
-           SAFE_HINTS="romio_no_indep_rw=true"
-        else
-           SAFE_HINTS="romio_no_indep_rw=false"
-        fi
-for mpiio_mode in 0 1 ; do
-        if test "$mpiio_mode" = 1 ; then
-           USEMPIO_HINTS="nc_pncio=disable"
-        else
-           USEMPIO_HINTS="nc_pncio=enable"
-        fi
-for intra_aggr in 0 1 ; do
-        if test "$intra_aggr" = 1 ; then
-           INA_HINTS="nc_num_aggrs_per_node=2"
-        else
-           INA_HINTS="nc_num_aggrs_per_node=0"
-        fi
+if test "x$MIMIC_LUSTRE" != x1 ; then
+   PNETCDF_HINTS="cb_nodes=2"
+fi
 
-        if [[ "$i" == *"vard"* ]] ; then
-           if test "x$mpiio_mode" == x0 || test "x$intra_aggr" == x1 ; then
-              # vard APIs are not supported when using PNCIO
-              continue
-           fi
-        fi
+for i in ${check_PROGRAMS} ; do
 
-        PNETCDF_HINTS=
-        if test "x$SAFE_HINTS" != x ; then
-           PNETCDF_HINTS="$SAFE_HINTS"
-        fi
-        if test "x$USEMPIO_HINTS" != x ; then
-           PNETCDF_HINTS="$USEMPIO_HINTS;$PNETCDF_HINTS"
-        fi
-        if test "x$INA_HINTS" != x ; then
-           PNETCDF_HINTS="$INA_HINTS;$PNETCDF_HINTS"
-        fi
+   exe_name=`basename $i`
 
-        export PNETCDF_HINTS="$PNETCDF_HINTS"
-        export PNETCDF_SAFE_MODE=$j
-	    # echo "PNETCDF_SAFE_MODE=$PNETCDF_SAFE_MODE PNETCDF_HINTS=$PNETCDF_HINTS"
+   for j in ${safe_modes} ; do
 
-    ${MPIRUN} ./test_inq_format ${srcdir}
-    ${MPIRUN} ./cdf_type ${TESTOUTDIR}/cdf_type.nc
-    ${MPIRUN} ./dim_cdf12 ${TESTOUTDIR}/dim_cdf12.nc
+      export PNETCDF_SAFE_MODE=$j
+      if test "x$VERBOSE" = xyes || test "x$DRY_RUN" = xyes ; then
+         echo "Line ${LINENO}: PNETCDF_SAFE_MODE=$PNETCDF_SAFE_MODE"
+      fi
 
-    # echo "--- validating file ${TESTOUTDIR}/cdf_type.nc"
-    ${TESTSEQRUN} ${VALIDATOR} -q ${TESTOUTDIR}/cdf_type.nc
-    # echo "--- validating file ${TESTOUTDIR}/dim_cdf12.nc"
-    ${TESTSEQRUN} ${VALIDATOR} -q ${TESTOUTDIR}/dim_cdf12.nc
+      if test "x$exe_name" = "xtest_inq_format" ; then
+         run_cmd ./$i -q -i ${srcdir}
+         continue
+      elif test "x$exe_name" = "xtst_corrupt" ; then
+         run_cmd ./$i ${srcdir}
+         continue
+      elif test "x$exe_name" = "xtst_open_cdf5" ; then
+         run_cmd ./$i ${srcdir}/bad_begin.nc5
+         continue
+      fi
 
-    if test "x${ENABLE_BURST_BUFFER}" = x1 ; then
-       # echo "---- test burst buffering feature"
-       saved_PNETCDF_HINTS=${PNETCDF_HINTS}
-       export PNETCDF_HINTS="${PNETCDF_HINTS};nc_burst_buf=enable;nc_burst_buf_dirname=${TESTOUTDIR};nc_burst_buf_overwrite=enable"
-       ${MPIRUN} ./test_inq_format ${srcdir}
-       ${MPIRUN} ./cdf_type ${TESTOUTDIR}/cdf_type.bb.nc
-       ${MPIRUN} ./dim_cdf12 ${TESTOUTDIR}/dim_cdf12.bb.nc
-       export PNETCDF_HINTS=${saved_PNETCDF_HINTS}
+      run_cmd ./$i -q -o ${TESTOUTDIR}/${exe_name}.nc
 
-       # echo "--- validating file ${TESTOUTDIR}/cdf_type.bb.nc"
-       ${TESTSEQRUN} ${VALIDATOR} -q ${TESTOUTDIR}/cdf_type.bb.nc
-       # echo "--- validating file ${TESTOUTDIR}/dim_cdf12.bb.nc"
-       ${TESTSEQRUN} ${VALIDATOR} -q ${TESTOUTDIR}/dim_cdf12.bb.nc
+   done # safe_modes
 
-       # echo "--- ncmpidiff cdf_type.nc cdf_type.bb.nc ---"
-       ${MPIRUN} ${NCMPIDIFF} -q ${TESTOUTDIR}/cdf_type.nc ${TESTOUTDIR}/cdf_type.bb.nc
-       # echo "--- ncmpidiff dim_cdf12.nc dim_cdf12.bb.nc ---"
-       # ${MPIRUN} ${NCMPIDIFF} -q ${TESTOUTDIR}/dim_cdf12.nc ${TESTOUTDIR}/dim_cdf12.bb.nc
-    fi
-done
-done
-done
-
-rm -f ${OUTDIR}/dim_cdf12.nc
-rm -f ${OUTDIR}/cdf_type.nc
-rm -f ${OUTDIR}/dim_cdf12.bb.nc
-rm -f ${OUTDIR}/cdf_type.bb.nc
+done # check_PROGRAMS
 
--- test/cdf_format/seq_runs.sh
+++ test/cdf_format/seq_runs.sh
@@ -1,32 +1,57 @@
-#!/bin/sh
+#!/bin/bash
 #
-# Copyright (C) 2003, Northwestern University and Argonne National Laboratory
+# Copyright (C) 2026, Northwestern University and Argonne National Laboratory
 # See COPYRIGHT notice in top-level directory.
 #
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+DRY_RUN=no
+VERBOSE=no
+
+run_cmd() {
+   local lineno=${BASH_LINENO[$((${#BASH_LINENO[@]} - 2))]}
+   if test "x$VERBOSE" = xyes || test "x$DRY_RUN" = xyes ; then
+      echo "Line $lineno CMD: $TESTSEQRUN $@"
+   fi
+   if test "x$DRY_RUN" = xno ; then
+      $TESTSEQRUN $@
+   fi
+}
+
+if test "x${PNETCDF_DEBUG}" = x1 ; then
+   safe_modes="0 1"
+else
+   safe_modes="0"
+fi
+
+exe_name=`basename $1`
+
 # prevent user environment setting of PNETCDF_HINTS to interfere
 unset PNETCDF_HINTS
 
-${TESTSEQRUN} ./test_inq_format ${srcdir}
+for j in ${safe_modes} ; do
 
-# the followings check files with corrupted header
-${TESTSEQRUN} ./tst_open_cdf5 ${srcdir}/bad_begin.nc5
-${TESTSEQRUN} ./tst_corrupt ${srcdir}
+   export PNETCDF_SAFE_MODE=$j
+   if test "x$VERBOSE" = xyes || test "x$DRY_RUN" = xyes ; then
+      echo "Line ${LINENO}: PNETCDF_SAFE_MODE=$PNETCDF_SAFE_MODE"
+   fi
 
-# echo ""
+   if test "x$exe_name" = xtest_inq_format ; then
+      run_cmd ./$1 -q -i ${srcdir}
+      continue
+   elif test "x$exe_name" = xtst_open_cdf5 ; then
+      # check files with corrupted header
+      run_cmd ./$1 ${srcdir}/bad_begin.nc5
+      continue
+   elif test "x$exe_name" = xtst_corrupt ; then
+      # check files with corrupted header
+      run_cmd ./$1 ${srcdir}
+      continue
+   fi
 
-if test "x${ENABLE_BURST_BUFFER}" = x1 ; then
-    echo ""
-    echo "---- testing burst buffering"
+   run_cmd ./$1 -q -o ${TESTOUTDIR}/${exe_name}.nc
 
-    export PNETCDF_HINTS="nc_burst_buf=enable;nc_burst_buf_dirname=${TESTOUTDIR};nc_burst_buf_overwrite=enable"
+done # safe_modes
 
-    ${TESTSEQRUN} ./test_inq_format ${srcdir}
-
-    # the followings check files with corrupted header
-    ${TESTSEQRUN} ./tst_open_cdf5 ${srcdir}/bad_begin.nc5
-    ${TESTSEQRUN} ./tst_corrupt ${srcdir}
-fi
