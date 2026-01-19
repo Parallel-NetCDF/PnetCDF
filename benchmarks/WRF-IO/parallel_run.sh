cfebccf024d54249ef5b9363a00c5d4caa844a48
wkliao <wkliao@users.noreply.github.com>
2026-01-18 20:38:46 -0600

    benchmarks/WRF-IO skip test no_indep_rw_hint and ptests on 4 and 6 processes

--- benchmarks/WRF-IO/parallel_run.sh
+++ benchmarks/WRF-IO/parallel_run.sh
@@ -5,7 +5,24 @@
 #
 
 # Exit immediately if a command exits with a non-zero status.
-set -e
+# set -e
+
+DRY_RUN=no
+VERBOSE=no
+
+run_cmd() {
+   local lineno=${BASH_LINENO[$((${#BASH_LINENO[@]} - 2))]}
+   if test "x$VERBOSE" = xyes || test "x$DRY_RUN" = xyes ; then
+      echo "Line $lineno CMD: $MPIRUN $@"
+   fi
+   if test "x$DRY_RUN" = xno ; then
+      $MPIRUN $@
+   fi
+   if test $? != 0 ; then
+      echo "FAIL: nprocs=$1 ---- $i $TEST_OPTS"
+      exit 1
+   fi
+}
 
 VALIDATOR=../../src/utils/ncvalidator/ncvalidator
 NCMPIDIFF=../../src/utils/ncmpidiff/ncmpidiff
@@ -30,11 +47,11 @@ fi
 # prevent user environment setting of PNETCDF_HINTS to interfere
 unset PNETCDF_HINTS
 
-fixed_length=23
-
 TEST_MPIIO_MODES="0 1"
 
 for i in ${check_PROGRAMS} ; do
+    # Capture start time in seconds and nanoseconds
+    start_time=$(date +%s.%1N)
 
     for j in ${safe_modes} ; do
         if test "$j" = 1 ; then # test only in safe mode
@@ -43,9 +60,6 @@ for i in ${check_PROGRAMS} ; do
            safe_hint="NOSAFE"
         fi
         OUT_PREFIX="${TESTOUTDIR}/$i"
-
-    for no_indep_rw in true false ; do
-        no_indep_rw_hint="romio_no_indep_rw=$no_indep_rw"
 
     for mpiio_mode in ${TEST_MPIIO_MODES} ; do
         if test "$mpiio_mode" = 1 ; then
@@ -71,7 +85,7 @@ for i in ${check_PROGRAMS} ; do
         OUT_FILE=$INA_OUT_FILE
         TEST_OPTS="$safe_hint $driver_hint $ina_hint"
 
-        PNETCDF_HINTS=$no_indep_rw_hint
+        PNETCDF_HINTS=
         if test "x$SAFE_HINTS" != x ; then
            PNETCDF_HINTS="$SAFE_HINTS;$PNETCDF_HINTS"
         fi
@@ -90,12 +104,7 @@ for i in ${check_PROGRAMS} ; do
         # echo "${LINENO}: PNETCDF_SAFE_MODE=$PNETCDF_SAFE_MODE PNETCDF_HINTS=$PNETCDF_HINTS"
 
         CMD_OPTS="-q -d -g -n 2 -y 100 -x 10 -i ${srcdir}/wrf_header.txt"
-        # echo "${LINENO}: ${MPIRUN} ./$i $CMD_OPTS -w $OUT_FILE.nc -r $OUT_FILE.nc"
-        ${MPIRUN} ./$i $CMD_OPTS -w $OUT_FILE.nc -r $OUT_FILE.nc
-
-        if test $? = 0 ; then
-           printf "PASS:  C  nprocs=$1 %-${fixed_length}s   -------- $i\n" "$TEST_OPTS"
-        fi
+        run_cmd ./$i $CMD_OPTS -w $OUT_FILE.nc -r $OUT_FILE.nc
 
         # echo "${LINENO}:--- validating file ${OUT_FILE}.nc"
         ${TESTSEQRUN} ${VALIDATOR} -q ${OUT_FILE}.nc
@@ -105,12 +114,7 @@ for i in ${check_PROGRAMS} ; do
            saved_PNETCDF_HINTS=${PNETCDF_HINTS}
            export PNETCDF_HINTS="${PNETCDF_HINTS};nc_burst_buf=enable;nc_burst_buf_dirname=${TESTOUTDIR};nc_burst_buf_overwrite=enable"
            # echo "${LINENO}: PNETCDF_HINTS=$PNETCDF_HINTS"
-           # echo "${LINENO}: ${MPIRUN} ./$i $CMD_OPTS -w $OUT_FILE.bb.nc -r $OUT_FILE.bb.nc"
-           ${MPIRUN} ./$i $CMD_OPTS -w $OUT_FILE.bb.nc -r $OUT_FILE.bb.nc
-
-           if test $? = 0 ; then
-              printf "PASS:  C  nprocs=$1 %-${fixed_length}s   -------- $i\n" "$TEST_OPTS BB"
-           fi
+           run_cmd ./$i $CMD_OPTS -w $OUT_FILE.bb.nc -r $OUT_FILE.bb.nc
 
            export PNETCDF_HINTS=${saved_PNETCDF_HINTS}
 
@@ -118,14 +122,12 @@ for i in ${check_PROGRAMS} ; do
            ${TESTSEQRUN} ${VALIDATOR} -q ${OUT_FILE}.bb.nc
 
            DIFF_OPT="-q"
-           # echo "${LINENO}: --- ncmpidiff $DIFF_OPT $OUT_FILE.nc $OUT_FILE.bb.nc ---"
-           ${MPIRUN} ${NCMPIDIFF} $DIFF_OPT $OUT_FILE.nc $OUT_FILE.bb.nc
+           run_cmd ${NCMPIDIFF} $DIFF_OPT $OUT_FILE.nc $OUT_FILE.bb.nc
         fi
 
         if test "x${ENABLE_NETCDF4}" = x1 ; then
            # echo "${LINENO}: test netCDF-4 feature"
-           # echo "${LINENO}: ${MPIRUN} ./$i $CMD_OPTS -w $OUT_FILE.nc -r $OUT_FILE.nc"
-           ${MPIRUN} ./$i $CMD_OPTS -w $OUT_FILE.nc4 -r $OUT_FILE.nc4
+           run_cmd ./$i $CMD_OPTS -w $OUT_FILE.nc4 -r $OUT_FILE.nc4
            # Validator does not support nc4
         fi
     done # intra_aggr
@@ -133,16 +135,21 @@ for i in ${check_PROGRAMS} ; do
 
     DIFF_OPT="-q"
     if test "x$TEST_MPIIO_MODES" = "x0 1" ; then
-       # echo "${LINENO}: --- ncmpidiff $OUT_PREFIX.mpio.nc $OUT_PREFIX.mpio.ina.nc ---"
-       $MPIRUN $NCMPIDIFF $DIFF_OPT $OUT_PREFIX.mpio.nc $OUT_PREFIX.mpio.ina.nc
-       # echo "${LINENO}: --- ncmpidiff $OUT_PREFIX.mpio.nc $OUT_PREFIX.pncio.nc ---"
-       $MPIRUN $NCMPIDIFF $DIFF_OPT $OUT_PREFIX.mpio.nc $OUT_PREFIX.pncio.nc
+       run_cmd $NCMPIDIFF $DIFF_OPT $OUT_PREFIX.mpio.nc $OUT_PREFIX.mpio.ina.nc
+       run_cmd $NCMPIDIFF $DIFF_OPT $OUT_PREFIX.mpio.nc $OUT_PREFIX.pncio.nc
     fi
-    # echo "${LINENO}: --- ncmpidiff $OUT_PREFIX.pncio.nc $OUT_PREFIX.pncio.ina.nc ---"
-    $MPIRUN $NCMPIDIFF $DIFF_OPT $OUT_PREFIX.pncio.nc $OUT_PREFIX.pncio.ina.nc
+    run_cmd $NCMPIDIFF $DIFF_OPT $OUT_PREFIX.pncio.nc $OUT_PREFIX.pncio.ina.nc
 
-    done # no_indep_rw
     done # safe_modes
     rm -f ${OUTDIR}/$i*nc*
+
+    end_time=$(date +%s.%1N)
+
+    # Calculate difference (requires bc for floating point math)
+    elapsed_time=$(echo "$end_time - $start_time" | bc)
+
+    fixed_length=48
+    printf "*** TESTING  %-${fixed_length}s   -- pass (%4ss)\n" "$i" "$elapsed_time"
+
 done # check_PROGRAMS
 
