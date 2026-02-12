4d0cc5540fe75acd21a921a9db25ca69712df754
wkliao <wkliao@users.noreply.github.com>
2026-02-11 18:33:39 -0600

    Update file create/open APIs for receive node IDs of all processes
    Node IDs and the number of compute nodes are generated during file
    creation or opening at the dispatcher.

--- src/drivers/ncbbio/ncbbio_driver.h
+++ src/drivers/ncbbio/ncbbio_driver.h
@@ -261,11 +261,13 @@ void ncbbio_export_hint (NC_bb *ncbbp, MPI_Info *info);
 
 extern
 int ncbbio_create(MPI_Comm comm, const char *path, int cmode, int ncid,
-                  int env_mode, MPI_Info info, void **ncdp);
+                  int env_mode, MPI_Info info, PNCIO_node_ids node_ids,
+                  void **ncdp);
 
 extern
 int ncbbio_open(MPI_Comm comm, const char *path, int omode, int ncid,
-                int env_mode, MPI_Info info, void **ncdp);
+                int env_mode, MPI_Info info, PNCIO_node_ids node_ids,
+                void **ncdp);
 
 extern int ncbbio_close (void *ncdp);
 
