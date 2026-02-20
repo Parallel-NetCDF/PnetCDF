2a9ef30a5019e8315e92c00ceddaef5aa3a0e8b1
wkliao <wkliao@users.noreply.github.com>
2026-02-20 15:52:46 -0600

    ncvalidator - remove safe mode loop, as Github CI has them in separate workflows
    Note configure with --enable-debug will enable safe mode
    Rename wrap_runs.sh to seq_runs.sh

--- src/utils/ncvalidator/seq_runs.sh
+++ src/utils/ncvalidator/seq_runs.sh
@@ -1,38 +1,32 @@
 #!/bin/sh
 #
-# Copyright (C) 2003, Northwestern University and Argonne National Laboratory
+# Copyright (C) 2017, Northwestern University and Argonne National Laboratory
 # See COPYRIGHT notice in top-level directory.
 #
 
 # Exit immediately if a command exits with a non-zero status.
-# set -e
-# Cannot use "set -e" here, as the tests here all return errors.
-
-VALIDATOR=ncvalidator
-if [ -z "${VALIDATOR}" ]; then
-   echo "Failed: variable VALIDATOR id not defined"
-   exit 1
-fi
-if [ ! -f ${VALIDATOR} ]; then
-   echo "Failed: file ${VALIDATOR} does not exit"
-   exit 1
-fi
+set -e
 
 # prevent user environment setting of PNETCDF_HINTS to interfere
 unset PNETCDF_HINTS
 
-for i in ${BAD_FILES} ; do
-    if [ ! -f ${srcdir}/$i ]; then
-       echo "Failed: input test file ${srcdir}/$i does not exit"
-       exit 1
-    fi
-    ${TESTSEQRUN} ./${VALIDATOR} ${srcdir}/$i
-    ret=$?
-    # capture exit status of VALIDATOR command
-    if [ ${ret} -ne 1 ]; then
-       echo "Failed: ${VALIDATOR} ${srcdir}/$i"
-       exit 1
-    fi
+for i in ${ENULLPAD_FILES} ; do
+    ${TESTSEQRUN} ./tst_open ${srcdir}/$i NC_ENULLPAD
 done
-echo "SUCCESS: ${VALIDATOR} to detect files fail to conform CDF formats"
+
+for i in ${EMAXVARS_FILES} ; do
+    ${TESTSEQRUN} ./tst_open ${srcdir}/$i NC_EMAXVARS
+done
+
+for i in ${EUNLIMIT_FILES} ; do
+    ${TESTSEQRUN} ./tst_open ${srcdir}/$i NC_EUNLIMIT
+done
+
+for i in ${ENOTNC_FILES} ; do
+    ${TESTSEQRUN} ./tst_open ${srcdir}/$i NC_ENOTNC
+done
+
+for i in ${EVARSIZE_FILES} ; do
+    ${TESTSEQRUN} ./tst_open ${srcdir}/$i NC_EVARSIZE
+done
 
