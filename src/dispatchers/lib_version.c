f4e34901c3062735974716cf88040d5cc3f476d2
Wei-keng Liao <wkliao@users.noreply.github.com>
2026-06-18 16:27:52 -0500

    use 128 characters for pnetcdf_libvers[] and pnetcdf_lib_vers[]

--- src/dispatchers/lib_version.c
+++ src/dispatchers/lib_version.c
@@ -14,29 +14,34 @@
  * "\044Id: \100(#) PnetCDF library version 1.4.0 of 16 Nov 2013 $"
  * in the library file (libpnetcdf.a).
  *
- * This string must be made a global variable. Otherwise, it won't work
- * when compiled with optimization options, e.g. -O2
+ * This string must be made a global variable. Otherwise, it won't work when
+ * compiled with optimization options, e.g. -O2
+ *
+ * Contents of string pnetcdf_libvers is slightly different from the one to be
+ * returned from ncmpi_inq_libvers(). pnetcdf_libvers is to be used for command
+ * "ident" to identify the RCS keyword strings. Note command "ident' looks for
+ * a specific keyword pattern and print it. See man page of ident.  One can run
+ * command "ident libfoo.a" to obtain the version string of library named foo
+ * (or an executable compiled from that library). In the PnetCDF case, the
+ * command "ident libpnetcdf.so" will print the contents of pnetcdf_libvers.
  */
-char const pnetcdf_libvers[] =
+char const pnetcdf_libvers[128] =
         "\044Id: \100(#) PnetCDF library version "PNETCDF_VERSION" of "PNETCDF_RELEASE_DATE" $";
 
-/* a cleaner version for running command "strings", e.g.
- * % strings libpnetcdf.a | grep "PnetCDF library version"
- * or
- * % strings a.out | grep "PnetCDF library version"
- */
-char const pnetcdf_lib_vers[] = "PnetCDF library version "PNETCDF_VERSION" of "PNETCDF_RELEASE_DATE;
 
-/* pnetcdf_libvers is slightly different from the one returned from
- * ncmpi_inq_libvers(). The string pnetcdf_libvers is for command "ident" to
- * use. People can run command ident libpnetcdf.a to obtain the version of a
- * library (or an executable built from that library). In PnetCDF case, the
- * command will print the string of pnetcdf_libvers. Command "ident' looks for
- * a specific keyword pattern and print it. See man page of ident.
- *
- * The API ncmpi_inq_libvers() below on the other hand returns a string to be
- * used by the utility tools like ncmpidump, ncmpigen, etc. Check the last line
- * of output from command "ncmpidump -v".
+/* Below pnetcdf_lib_vers is a cleaner version of pnetcdf_libvers. It is for
+ * running command "strings", e.g.
+ *    % strings libpnetcdf.a | grep "^PnetCDF library version"
+ * or
+ *    % strings a.out | grep "^PnetCDF library version"
+ */
+char const pnetcdf_lib_vers[128] =
+        "PnetCDF library version "PNETCDF_VERSION" of "PNETCDF_RELEASE_DATE;
+
+/* The API ncmpi_inq_libvers() below returns the contents of pnetcdf_lib_vers,
+ * instead of pnetcdf_libvers, which is used by the PnetCDF utility tools like
+ * ncmpidump and ncmpidiff. Check the last line from the output of commands
+ * "ncmpidump -h" and "ncmpidiff -h".
  */
 
 /*----< ncmpi_inq_libvers() >------------------------------------------------*/
